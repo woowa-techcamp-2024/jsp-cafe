3297b164f5d707fd2ee4f72e4a39cba49bbcbae5
KoKimSS <seungsu8848@ajou.ac.kr>
2024-07-22 14:57:52 +0900

    init : 프로젝트 초기화

---
+++ src/main/java/org/example/jspcafe/HelloServlet.java
@@ -0,0 +1,27 @@
+package org.example.jspcafe;
+
+import java.io.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet2")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World! 22";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
