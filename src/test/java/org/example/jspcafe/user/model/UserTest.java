b1e8232cfb88a49062d014b6d75a9f5a6be9bf64
miiiinju1 <gms07073@ynu.ac.kr>
2024-07-22 23:18:42 +0900

    test: User 비밀번호 변경 테스트 작성

--- src/test/java/org/example/jspcafe/user/model/UserTest.java
+++ src/test/java/org/example/jspcafe/user/model/UserTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class UserTest {
 
@@ -24,4 +25,39 @@ class UserTest {
                 .containsExactly(null, nickname, email, password);
     }
 
+    @DisplayName("비밀번호 변경 테스트")
+    @Test
+    void updatePassword() {
+        // given
+        String nickname = "nickname";
+        String email = "email@example.com";
+        String password = "password";
+
+        User user = new User(nickname, email, password);
+
+        String newPassword = "newPassword";
+
+        // when
+        user.updatePassword(newPassword);
+
+        // then
+        assertThat(user.getPassword().getValue()).isEqualTo(newPassword);
+
+    }
+
+    @DisplayName("같은 비밀번호로 변경하면 예외가 발생한다")
+    @Test
+    void updatePasswordSamePassword() {
+        // given
+        String nickname = "nickname";
+        String email = "eamil@example.com";
+        String password = "password";
+
+        User user = new User(nickname, email, password);
+
+        // when & then
+        assertThatThrownBy(() -> user.updatePassword(password))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("기존 비밀번호와 동일합니다.");
+    }
 }
