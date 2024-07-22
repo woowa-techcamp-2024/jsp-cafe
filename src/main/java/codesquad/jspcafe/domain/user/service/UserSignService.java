46c7827cf7ba9e88c2727cdee1256159937e672c
KyungMin Lee <hello.tidavid@gmail.com>
2024-07-22 20:11:12 +0900

    feat: 사용자 회원 가입 기능 구현
    - UserSignService의 `createUser()`를 통해 회원 가입을 진행할 수 있습니다.
    - 가입된 회원은 UserRepository에 저장됩니다.

---
+++ src/main/java/codesquad/jspcafe/domain/user/service/UserSignService.java
@@ -0,0 +1,24 @@
+package codesquad.jspcafe.domain.user.service;
+
+import codesquad.jspcafe.domain.user.domain.User;
+import codesquad.jspcafe.domain.user.repository.UserRepository;
+import java.util.Map;
+
+public class UserSignService {
+
+    private final UserRepository userRepository;
+
+    public UserSignService() {
+        userRepository = new UserRepository();
+    }
+
+    public User createUser(Map<String, String[]> parameterMap) {
+        String userId = parameterMap.get("userId")[0];
+        String password = parameterMap.get("password")[0];
+        String name = parameterMap.get("name")[0];
+        String email = parameterMap.get("email")[0];
+        User user = new User(userId, password, name, email);
+        return userRepository.save(user);
+    }
+
+}
