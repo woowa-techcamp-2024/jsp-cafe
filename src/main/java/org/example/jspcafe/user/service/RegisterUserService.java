7263d41400959134a4dc869905d74f0feaa91c03
miiiinju1 <gms07073@ynu.ac.kr>
2024-07-22 23:37:19 +0900

    feat: 회원가입한 User를 저장하는 RegisterUserService 구현

---
+++ src/main/java/org/example/jspcafe/user/service/RegisterUserService.java
@@ -0,0 +1,27 @@
+package org.example.jspcafe.user.service;
+
+import org.example.jspcafe.Repository;
+import org.example.jspcafe.user.model.User;
+import org.example.jspcafe.user.request.RegisterUserServiceRequest;
+
+public class RegisterUserService {
+
+    private final Repository<User> userRepository;
+
+    public void registerUser(final RegisterUserServiceRequest request) {
+        final String nickname = request.nickname();
+        final String email = request.email();
+        final String password = request.password();
+
+        User user = new User(nickname, email, password);
+
+        userRepository.save(user);
+        System.out.println("user = " + user.getNickname() + " " + user.getEmail());
+
+    }
+
+
+    public RegisterUserService(Repository<User> userRepository) {
+        this.userRepository = userRepository;
+    }
+}
