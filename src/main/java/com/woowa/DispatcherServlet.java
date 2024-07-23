d756eea87d77c8b8e174286404c329da093f3e46
hseong3243 <hseong3243@gmail.com>
2024-07-23 12:30:33 +0900

    feat: 디스패처 서블릿 구현
    사용자의 요청 경로에 따라 실행할 메서드를 동적으로 매핑함.
    ArgumentResolver를 이용하여 요청할 메서드 파라미터에 맞는 인자를 resolve 함.

--- src/main/java/com/woowa/DispatcherServlet.java
+++ src/main/java/com/woowa/DispatcherServlet.java
@@ -1,14 +1,63 @@
 package com.woowa;
 
-import jakarta.servlet.ServletException;
+import com.woowa.framework.BeanFactory;
+import com.woowa.framework.argumentresovler.ArgumentResolverComposite;
+import com.woowa.framework.web.HandlerMapping;
+import com.woowa.framework.web.HandlerMethod;
+import com.woowa.framework.web.ResponseEntity;
+import com.woowa.framework.web.StaticResourceHandler;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
+import java.util.Map.Entry;
 
 public class DispatcherServlet extends HttpServlet {
+
+    private final HandlerMapping handlerMapping;
+    private ArgumentResolverComposite argumentResolvers = new ArgumentResolverComposite();
+
+    public DispatcherServlet(HandlerMapping handlerMapping) {
+        this.handlerMapping = handlerMapping;
+    }
+
+    public void init(BeanFactory beanFactory) {
+        argumentResolvers = beanFactory.getBean(ArgumentResolverComposite.class);
+    }
+
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("Hello Servlet");
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            dispatch(req, resp);
+        } catch (Throwable e) {
+            //todo 에러핸들링
+        }
+    }
+
+    private void dispatch(HttpServletRequest req, HttpServletResponse resp) throws Throwable {
+        if(StaticResourceHandler.supports(req)) {
+            byte[] body = StaticResourceHandler.getResource(req);
+            resp.setContentLength(body.length);
+            resp.setContentType("text/html");
+            resp.getOutputStream().write(body);
+            resp.getOutputStream().flush();
+        }
+
+        HandlerMethod handlerMethod = handlerMapping.getHandlerMethod(req);
+        Parameter[] parameters = handlerMethod.getParameters();
+        Object[] args = new Object[parameters.length];
+        for (int i = 0; i < parameters.length; i++) {
+            Parameter parameter = parameters[i];
+            args[i] = argumentResolvers.resolveArgument(parameter, req, resp);
+        }
+
+        ResponseEntity responseEntity = handlerMethod.invoke(args);
+
+        resp.setStatus(responseEntity.getStatus());
+        for (Entry<String, String> entry : responseEntity.getHeader().entrySet()) {
+            resp.addHeader(entry.getKey(), entry.getValue());
+        }
+        resp.getWriter().print(Arrays.toString(responseEntity.getBody()));
     }
 }
--- src/main/java/com/woowa/DispatcherServletContainer.java
+++ src/main/java/com/woowa/DispatcherServletContainer.java
@@ -1,15 +1,31 @@
 package com.woowa;
 
+import com.woowa.framework.BeanFactory;
+import com.woowa.framework.argumentresovler.ArgumentResolverComposite;
+import com.woowa.framework.web.HandlerMapping;
 import jakarta.servlet.ServletContainerInitializer;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRegistration.Dynamic;
 import java.util.Set;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DispatcherServletContainer implements ServletContainerInitializer {
+    private static final Logger log = LoggerFactory.getLogger(DispatcherServletContainer.class);
+
     @Override
     public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
-        Dynamic helloServlet = ctx.addServlet("helloServlet", new DispatcherServlet());
+        BeanFactory beanFactory = new BeanFactory();
+        beanFactory.start();
+
+        HandlerMapping handlerMapping = new HandlerMapping(beanFactory);
+        handlerMapping.init();
+
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(handlerMapping);
+        dispatcherServlet.init(beanFactory);
+
+        Dynamic helloServlet = ctx.addServlet("helloServlet", dispatcherServlet);
         helloServlet.addMapping("/*");
     }
 }
