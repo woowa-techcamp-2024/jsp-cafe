e0fb86afe75af29081af1f1d4a6277aeeff85318
kimhyun5u <22kimhyun5u@gmail.com>
2024-07-23 15:12:01 +0900

    test: QuestionRepository findAll Test 추가

--- src/test/java/repository/QuestionRepositoryTest.java
+++ src/test/java/repository/QuestionRepositoryTest.java
@@ -1,11 +1,21 @@
 package repository;
 
+import camp.woowa.jspcafe.models.Question;
 import camp.woowa.jspcafe.repository.InMemQuestionRepository;
+import camp.woowa.jspcafe.repository.QuestionRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class QuestionRepositoryTest {
+    QuestionRepository questionRepository;
+    @BeforeEach
+    void setUp() {
+        questionRepository = new InMemQuestionRepository();
+    }
     @Test
     void TestSave() {
         // given
@@ -15,9 +25,26 @@ class QuestionRepositoryTest {
         Long expectedId = 1L;
 
         // when
-        Long id = new InMemQuestionRepository().save(title, content, writer);
+        Long id = questionRepository.save(title, content, writer);
 
         // then
         assertEquals(expectedId, id);
+    }
+
+    @Test
+    void findAll() {
+        // given
+        String title = "title";
+        String content = "content";
+        String writer = "1234";
+        questionRepository.save(title, content, writer);
+
+        int expectedSize = 1;
+
+        // when
+        List<Question> questions = questionRepository.findAll();
+
+        // then
+        assertEquals(expectedSize, questions.size());
     }
 }
