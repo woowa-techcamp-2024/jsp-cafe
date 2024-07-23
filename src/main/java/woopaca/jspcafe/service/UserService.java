f6f0cd8beaa708e0c819b4a851f2372a31cc39fc
jcw1031 <jcw001031@gmail.com>
2024-07-23 16:16:51 +0900

    feat: 사용자 회원가입 로직
    - UserService 추가
    - 회원가입 요청 데이터 검증

---
+++ src/main/java/woopaca/jspcafe/service/UserService.java
@@ -0,0 +1,29 @@
+package woopaca.jspcafe.service;
+
+import woopaca.jspcafe.model.User;
+import woopaca.jspcafe.repository.UserMemoryRepository;
+import woopaca.jspcafe.repository.UserRepository;
+import woopaca.jspcafe.servlet.dto.SignUpRequest;
+
+public class UserService {
+
+    private final UserRepository userRepository = new UserMemoryRepository();
+
+    public void signUp(SignUpRequest signUpRequest) {
+        validateSignUpRequest(signUpRequest);
+        User user = new User(signUpRequest.username(), signUpRequest.nickname(), signUpRequest.password());
+        userRepository.save(user);
+    }
+
+    private void validateSignUpRequest(SignUpRequest signUpRequest) {
+        if (signUpRequest.username().isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 이메일은 비어있을 수 없습니다.");
+        }
+        if (signUpRequest.nickname().length() < 2) {
+            throw new IllegalArgumentException("[ERROR] 닉네임은 비어있을 수 없습니다.");
+        }
+        if (signUpRequest.password().length() < 4) {
+            throw new IllegalArgumentException("[ERROR] 비밀번호는 비어있을 수 없습니다.");
+        }
+    }
+}
