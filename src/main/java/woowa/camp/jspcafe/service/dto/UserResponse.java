835ad87e323c98cdcdf846a36ee0b92ccee76ef1
june-777 <wlwhswnsrl96@gmail.com>
2024-07-24 02:15:15 +0900

    feat: 사용자 조회 응답 Response에 회원가입 일자 추가

--- src/main/java/woowa/camp/jspcafe/service/dto/UserResponse.java
+++ src/main/java/woowa/camp/jspcafe/service/dto/UserResponse.java
@@ -1,11 +1,12 @@
 package woowa.camp.jspcafe.service.dto;
 
+import java.time.LocalDate;
 import woowa.camp.jspcafe.domain.User;
 
-public record UserResponse(Long id, String nickname, String email) {
+public record UserResponse(Long id, String nickname, String email, LocalDate registerAt) {
 
     public static UserResponse of(User user) {
-        return new UserResponse(user.getId(), user.getNickname(), user.getEmail());
+        return new UserResponse(user.getId(), user.getNickname(), user.getEmail(), user.getRegisterAt());
     }
 
 }
