786596326cb30d9dcbd9c0577eb7df62218e16cb
huiseung <whtjddn75@gmail.com>
2024-07-23 12:28:09 +0900

    [feature] 사용자 목록 서블릿

---
+++ src/main/java/org/example/servlet/view/UserListView.java
@@ -0,0 +1,32 @@
+package org.example.servlet.view;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.domain.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/users")
+public class UserListView extends HttpServlet {
+    private final Logger log = LoggerFactory.getLogger(UserListView.class);
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        log.debug("[UserListView] called");
+        List<User> users = new ArrayList<>();
+        users.add(new User("hong@example.com", "hong", "a", LocalDateTime.now()));
+        users.add(new User("kim@example.com", "kim",  "a", LocalDateTime.now()));
+        users.add(new User("lee@example.com", "lee", "a", LocalDateTime.now()));
+
+        request.setAttribute("users", users);
+        request.getRequestDispatcher("/user/list.jsp").forward(request, response);
+    }
+}
