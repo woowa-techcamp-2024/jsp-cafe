d0cb4b0b679807ff7f93016dbb7e2178751c8fa3
hseong3243 <hseong3243@gmail.com>
2024-07-23 12:28:41 +0900

    feat: 핸들러 메서드 응답 클래스에 필드 추가
    헤더, 바디를 추가하고 빌더 클래스에 반영함.

--- src/main/java/com/woowa/framework/web/ResponseEntity.java
+++ src/main/java/com/woowa/framework/web/ResponseEntity.java
@@ -5,18 +5,20 @@ import java.util.Map;
 
 public class ResponseEntity {
 
-    private final Map<String, Object> model;
+    private final Map<String, Object> model = new HashMap<>();
+    private final Map<String, String> header = new HashMap<>();
     private final int status;
     private String location = "";
+    private final byte[] body;
 
-    public ResponseEntity(Map<String, Object> model, int status) {
-        this(model, status, "");
+    public ResponseEntity(int status, byte[] body) {
+        this(status, "", body);
     }
 
-    public ResponseEntity(Map<String, Object> model, int status, String location) {
-        this.model = model;
+    public ResponseEntity(int status, String location, byte[] body) {
         this.status = status;
         this.location = location;
+        this.body = body;
     }
 
     public void add(String key, Object value) {
@@ -30,19 +32,43 @@ public class ResponseEntity {
     public static class Builder {
 
         private Map<String, Object> model = new HashMap<>();
-        private int status = 200;
+        private Map<String, String> header = new HashMap<>();
 
         public Builder add(String key, Object value) {
             model.put(key, value);
             return this;
         }
 
-        public ResponseEntity created() {
-            return new ResponseEntity(model, 201);
+        public Builder addHeader(String key, String value) {
+            header.put(key, value);
+            return this;
         }
 
         public ResponseEntity found(String location) {
-            return new ResponseEntity(model, 302, location);
+            ResponseEntity responseEntity = new ResponseEntity(302, location, new byte[0]);
+            responseEntity.model.putAll(model);
+            responseEntity.header.putAll(header);
+            return responseEntity;
         }
+    }
+
+    public Map<String, Object> getModel() {
+        return model;
+    }
+
+    public Map<String, String> getHeader() {
+        return header;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public byte[] getBody() {
+        return body;
     }
 }
