4285f77b8d79d03343bb5827a45b05950f2531eb
bellringstar <bellringstar@gmail.com>
2024-07-23 15:34:39 +0900

    feat: Post
    Post 저장을 위한 dao

--- src/main/java/org/example/post/model/dao/Post.java
+++ src/main/java/org/example/post/model/dao/Post.java
@@ -2,6 +2,7 @@ package org.example.post.model.dao;
 
 public class Post {
 
+    private Long id;
     private String writer;
     private String title;
     private String contents;
@@ -12,6 +13,19 @@ public class Post {
         post.title = title;
         post.contents = contents;
         return post;
+    }
+
+    public static Post createWithId(Long id, String writer, String title, String contents) {
+        Post post = new Post();
+        post.id = id;
+        post.writer = writer;
+        post.title = title;
+        post.contents = contents;
+        return post;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getWriter() {
