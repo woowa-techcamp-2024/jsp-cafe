ff9f47f8f26e4a700e00060cb500389d459c2150
bellringstar <bellringstar@gmail.com>
2024-07-23 12:13:06 +0900

    feat: DataUtil
    명시적으로 드라이버 로딩

--- src/main/java/org/example/util/DataUtil.java
+++ src/main/java/org/example/util/DataUtil.java
@@ -9,9 +9,15 @@ public class DataUtil {
     private static final String USER = "root";
     private static final String PASSWORD = "root";
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL);
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new ExceptionInInitializerError("MySQL JDBC Driver not found. " + e.getMessage());
+        }
     }
 
-
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
