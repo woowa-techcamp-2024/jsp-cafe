754dffe2b2e35acbd4134fa602a77b1989f83018
miiiinju1 <gms07073@ynu.ac.kr>
2024-07-23 22:09:15 +0900

    test: PostListResponse 레코드 테스트 추가

---
+++ src/test/java/org/example/jspcafe/post/response/PostListResponseTest.java
@@ -0,0 +1,32 @@
+package org.example.jspcafe.post.response;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PostListResponseTest {
+
+    @DisplayName("PostListResponse 생성 테스트")
+    @Test
+    void createPostListResponse() {
+        // given
+        List<PostResponse> postList = List.of(
+                new PostResponse(1L, "nickname", "title", "content", null),
+                new PostResponse(2L, "nickname", "title", "content", null)
+        );
+        int totalElements = postList.size();
+
+        // when
+        PostListResponse postListResponse = PostListResponse.of(totalElements, postList);
+
+        // then
+        assertThat(postListResponse)
+                .extracting("totalElements", "count", "postList")
+                .containsExactly(totalElements, postList.size(), postList);
+
+    }
+
+}
