d2a50e2ca25cf84f5d35afed51115f43089b20d0
kimhyun5u <22kimhyun5u@gmail.com>
2024-07-23 10:15:29 +0900

    docs: InMemUserRepository 주석 추가

--- src/main/java/camp/woowa/jspcafe/repository/InMemUserRepository.java
+++ src/main/java/camp/woowa/jspcafe/repository/InMemUserRepository.java
@@ -7,7 +7,10 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class InMemUserRepository implements UserRepository {
+
+    // id, User 로 생성한 맵을 데이터 베이스처럼 사용한다.
     private static final Map<String, User> users = new ConcurrentHashMap<>();
+
     @Override
     public String save(String userId, String password, String name, String email) {
         users.put(userId, new User(userId, password, name, email));
