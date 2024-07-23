cc0147b69315a9c71bca8d25096f566cc3000333
KyuWon  Kim <kyuwon0718@gmail.com>
2024-07-23 11:15:15 +0900

    feat: 유저 회원가입, 유저 목록 조회 서블릿 추가

---
+++ src/main/java/org/example/demo/UsersServlet.java
@@ -0,0 +1,25 @@
+package org.example.demo;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "userCreateServlet", urlPatterns = "/users")
+public class UsersServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        response.sendRedirect("/user/list.html");
+        System.out.println("do get /users");
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        response.sendRedirect("/user/list.html");
+        System.out.println("do post /users");
+    }
+}
