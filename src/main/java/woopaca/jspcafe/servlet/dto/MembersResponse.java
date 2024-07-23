693cd173cd27c0859a19ae58703133a59d68fd7d
jcw1031 <jcw001031@gmail.com>
2024-07-23 20:50:02 +0900

    refactor: 사용자 목록 DTO에 id 속성 추가
    - 사용자 목록에서 사용자 프로필 페이지 이동을 위해 추가

--- src/main/java/woopaca/jspcafe/servlet/dto/MembersResponse.java
+++ src/main/java/woopaca/jspcafe/servlet/dto/MembersResponse.java
@@ -2,9 +2,9 @@ package woopaca.jspcafe.servlet.dto;
 
 import woopaca.jspcafe.model.User;
 
-public record MembersResponse(String nickname, String email, String createdAt) {
+public record MembersResponse(String id, String nickname, String email, String createdAt) {
 
     public static MembersResponse from(User user) {
-        return new MembersResponse(user.getNickname(), user.getUsername(), user.getCreatedAt().toString());
+        return new MembersResponse(user.getId(), user.getNickname(), user.getUsername(), user.getCreatedAt().toString());
     }
 }
