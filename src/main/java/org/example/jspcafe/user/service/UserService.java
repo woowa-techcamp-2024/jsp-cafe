3be9d413bdfc4f1d08603725a9b3ac7bdbca03d7
miiiinju1 <gms07073@ynu.ac.kr>
2024-07-23 17:07:07 +0900

    feat: 닉네임을 변경하는 EditProfile 추가

--- src/main/java/org/example/jspcafe/user/service/UserService.java
+++ src/main/java/org/example/jspcafe/user/service/UserService.java
@@ -1,6 +1,7 @@
 package org.example.jspcafe.user.service;
 
 import org.example.jspcafe.Component;
+import org.example.jspcafe.user.model.User;
 import org.example.jspcafe.user.repository.InMemoryUserRepository;
 import org.example.jspcafe.user.repository.UserRepository;
 import org.example.jspcafe.user.response.UserListResponse;
@@ -29,5 +30,17 @@ public class UserService {
         return userRepository.findByNickname(nickname)
                 .map(user -> new UserProfileResponse(user.getNickname().getValue(), user.getEmail().getValue(), user.getCreatedAt()))
                 .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
+    }
+
+    public void editProfile(Long userId, String newNickname, String password) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
+
+        if(!user.getPassword().getValue().equals(password)) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+        user.updateNickname(newNickname);
+
+        userRepository.update(user);
     }
 }
