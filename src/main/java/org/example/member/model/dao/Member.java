705211a67a827f90c85c06a06fac9def561ec312
bellringstar <bellringstar@gmail.com>
2024-07-23 10:49:13 +0900

    feat: Member
    회원 정보 저장을 위한 dao 작성. 생성자 메서드 추가

---
+++ src/main/java/org/example/member/model/dao/Member.java
@@ -0,0 +1,39 @@
+package org.example.member.model.dao;
+
+public class Member {
+
+    private String userId;
+    private String password;
+    private String name;
+    private String email;
+
+    public static Member createUser(String userId, String password, String name, String email) {
+        Member member = new Member();
+        member.userId = userId;
+        member.password = password;
+        member.name = name;
+        member.email = email;
+
+        return member;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    private void validate() {
+
+    }
+}
