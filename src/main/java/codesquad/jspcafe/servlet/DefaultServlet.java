62ce984013c18ebc790ed8e4d443cb237c91bae0
KyungMin Lee <hello.tidavid@gmail.com>
2024-07-23 13:04:21 +0900

    feat: .jsp 연결
    - *.html 요청에 대해 .jsp 로 응답하기 위한 서블릿을 구현했습니다.

---
+++ src/main/java/codesquad/jspcafe/servlet/DefaultServlet.java
@@ -0,0 +1,19 @@
+package codesquad.jspcafe.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("*.html")
+public class DefaultServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException {
+        String path = req.getRequestURI().replace(".html", ".jsp");
+        req.getRequestDispatcher(path).forward(req, resp);
+    }
+}
