2f23136d44c61f3e2f455e31a36275e527e15b91
lass9436 <lass9436@gmail.com>
2024-07-23 15:38:06 +0900

    [repository] UserRepository 인터페이스 작성
    유저 정보를 저장하는 UserRepository 인터페이스를 작성하였습니다.
    CRUD 메서드를 작성하였습니다.

---
+++ src/main/java/lass9436/user/model/UserRepository.java
@@ -0,0 +1,13 @@
+package lass9436.user.model;
+
+import java.util.List;
+
+public interface UserRepository {
+	User save(User user); // Create or Update
+
+	User findByUserId(String userId); // Read by ID
+
+	List<User> findAll(); // Read all
+
+	void deleteByUserId(String userId); // Delete by ID
+}
