e14a0590406c1eff45d146453863f0904a13f292
huiseung <whtjddn75@gmail.com>
2024-07-23 12:28:02 +0900

    [feature] 사용자 프로필 서블릿

---
+++ src/main/java/org/example/servlet/view/UserProfileView.java
@@ -0,0 +1,30 @@
+package org.example.servlet.view;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.domain.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@WebServlet(urlPatterns = {"/users/*"})
+public class UserProfileView extends HttpServlet {
+    private final Logger log = LoggerFactory.getLogger(UserProfileView.class);
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        log.debug("[UserProfileView] called");
+        String pathInfo = request.getPathInfo();
+        String userId = pathInfo.substring(1);
+        log.debug("[UserProfileView] "+userId);
+        User user = new User("hong@example.com", "hong", "a", LocalDateTime.now());
+
+        request.setAttribute("user", user);
+        request.getRequestDispatcher("/user/profile.jsp").forward(request, response);
+    }
+}
