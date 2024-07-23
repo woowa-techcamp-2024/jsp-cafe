fb042e49415ceb25faf7b7ac0cf8aabec946dcfd
KyuWon  Kim <kyuwon0718@gmail.com>
2024-07-23 13:03:52 +0900

    rename: user/list.html -> user/list.jsp

--- src/main/java/org/example/demo/UsersServlet.java
+++ src/main/java/org/example/demo/UsersServlet.java
@@ -1,5 +1,6 @@
 package org.example.demo;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,17 +10,28 @@ import java.io.IOException;
 
 @WebServlet(name = "usersServlet", urlPatterns = "/users")
 public class UsersServlet extends HttpServlet {
+    // 회원 목록 조회
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
-        response.sendRedirect("/user/list.html");
+        response.sendRedirect("/user/list.jsp");
         System.out.println("do get /users");
     }
 
+    // 회원가입
     @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-        response.sendRedirect("/user/list.html");
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        User user = new User(
+                request.getParameter("userId"),
+                request.getParameter("password"),
+                request.getParameter("name"),
+                request.getParameter("email"));
+
+        UserDb.addUser(user);
+
+        request.setAttribute("users", UserDb.getUsers());
+        System.out.println(UserDb.getUsers());
+        request.getRequestDispatcher("/user/list.jsp").forward(request, response);
         System.out.println("do post /users");
     }
 }
