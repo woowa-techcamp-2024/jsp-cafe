43107163d124b2c63b301e051265074551ce3c60
kimhyun5u <22kimhyun5u@gmail.com>
2024-07-23 10:19:48 +0900

    test: UserService findAll 테스트 추가

--- src/test/java/services/UserServiceTest.java
+++ src/test/java/services/UserServiceTest.java
@@ -4,12 +4,22 @@ import camp.woowa.jspcafe.models.User;
 import camp.woowa.jspcafe.repository.InMemUserRepository;
 import camp.woowa.jspcafe.repository.UserRepository;
 import camp.woowa.jspcafe.services.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UserServiceTest {
-    UserRepository userRepository = new InMemUserRepository();
+    UserRepository userRepository;
+    UserService userService;
+
+    @BeforeEach
+    void setUp() {
+        userRepository = new InMemUserRepository();
+        userService = new UserService(userRepository);
+    }
 
     @Test
     void testCreateUser() {
@@ -18,11 +28,23 @@ class UserServiceTest {
         String password = "password";
         String name = "name";
         String email = "email";
-        UserService userService = new UserService(userRepository);
         // when
         String id = userService.createUser(userId, password, name, email);
 
         // then
         assertEquals(id, userId);
+    }
+
+    @Test
+    void testFindAll() {
+        // given
+        testCreateUser();
+        int expected_size = 1;
+
+        // when
+        List<User> users = userService.findAll();
+
+        // then
+        assertEquals(users.size(), expected_size);
     }
 }
