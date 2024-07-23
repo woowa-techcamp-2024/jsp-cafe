af8a94348a8ae76b9c0ed1770d97be1db608f569
kariskan <zkfzpf56@naver.com>
2024-07-24 00:33:43 +0900

    feat: database init sql 실행

--- src/main/java/com/example/context/ApplicationInitializer.java
+++ src/main/java/com/example/context/ApplicationInitializer.java
@@ -1,7 +1,15 @@
 package com.example.context;
 
-import com.example.db.ArticleMemoryDatabase;
-import com.example.db.UserMemoryDatabase;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+import com.example.db.ArticleMysqlDatabase;
+import com.example.db.UserMysqlDatabase;
 
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
@@ -12,7 +20,34 @@ public class ApplicationInitializer implements ServletContextListener {
 
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
-		sce.getServletContext().setAttribute("userDatabase", new UserMemoryDatabase());
-		sce.getServletContext().setAttribute("articleDatabase", new ArticleMemoryDatabase());
+		sce.getServletContext().setAttribute("userDatabase", new UserMysqlDatabase());
+		sce.getServletContext().setAttribute("articleDatabase", new ArticleMysqlDatabase());
+		String url = "jdbc:mysql://localhost:3306/codesquad";
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+
+		try (
+			Connection connection = DriverManager.getConnection(url, "root", "root");
+			Statement statement = connection.createStatement();
+			InputStream inputStream = getClass().getClassLoader().getResourceAsStream("schema.sql")) {
+			if (inputStream == null) {
+				throw new RuntimeException("not found schema.sql");
+			}
+			Scanner scanner = new Scanner(inputStream);
+			scanner.useDelimiter(";");
+			while (scanner.hasNext()) {
+				String sql = scanner.next().trim();
+				if (!sql.isEmpty()) {
+					statement.execute(sql);
+				}
+			}
+			scanner.close();
+		} catch (SQLException | IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
