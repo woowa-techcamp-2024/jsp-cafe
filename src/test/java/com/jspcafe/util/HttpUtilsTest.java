700a45fab1f1e70490045feaff8dd7a47a0937e1
hellomatia <shuaigejp80@gmail.com>
2024-07-24 00:14:03 +0900

    feat: HttpRequest의 Body를 읽고, Json으로 파싱해주는 기능 추가
    - Jackson라이브러리를 사용하여 Json형식 데이터를 Map형태로 반환한다.
    - 테스트를 위해 HttpRequest의 간단한 스텁을 생성하여 테스트를 진행한다.

---
+++ src/test/java/com/jspcafe/util/HttpUtilsTest.java
@@ -0,0 +1,42 @@
+package com.jspcafe.util;
+
+import jakarta.servlet.http.*;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class HttpUtilsTest {
+    @Test
+    void HttpRequest_body_내용을_정상적으로_반환한다() throws IOException {
+        // Given
+        String expectedBody = "This is the request body";
+        HttpServletRequest request = new StubHttpServletRequest(expectedBody);
+
+        // When
+        String actualBody = HttpUtils.readRequestBody(request);
+
+        // Then
+        assertEquals(expectedBody, actualBody);
+    }
+
+    @Test
+    void JSON_형식의_요청_본문을_Map으로_변환한다() throws IOException {
+        // Given
+        String jsonBody = "{\"name\":\"John Doe\",\"age\":30,\"city\":\"New York\"}";
+        HttpServletRequest request = new StubHttpServletRequest(jsonBody);
+
+        // When
+        Map<String, Object> result = HttpUtils.getJsonRequestBody(request);
+
+        // Then
+        assertNotNull(result);
+        assertEquals(3, result.size());
+        assertEquals("John Doe", result.get("name"));
+        assertEquals(30, result.get("age"));
+        assertEquals("New York", result.get("city"));
+    }
+}
