12d93bd7ed6d413af0f2c56bc6b05d25daf36c37
june-777 <wlwhswnsrl96@gmail.com>
2024-07-24 02:16:15 +0900

    feat: 고정된 날짜를 반환하는 FixedDateTimeProvider 테스트용 구현체

---
+++ src/test/java/woowa/camp/jspcafe/utils/FixedDateTimeProvider.java
@@ -0,0 +1,20 @@
+package woowa.camp.jspcafe.utils;
+
+import java.time.LocalDate;
+import woowa.camp.jspcafe.utils.time.DateTimeProvider;
+
+public class FixedDateTimeProvider implements DateTimeProvider {
+
+    LocalDate fixedTime;
+
+    public FixedDateTimeProvider(int year, int month, int day) {
+        fixedTime = LocalDate.of(year, month, day);
+    }
+
+    @Override
+    public LocalDate getNow() {
+        return fixedTime;
+    }
+
+}
+
