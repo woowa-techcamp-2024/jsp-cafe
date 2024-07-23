00f23e68fae7a4060d665d167f13ab31f72dcd1d
kimhyun5u <22kimhyun5u@gmail.com>
2024-07-23 15:16:43 +0900

    feat: WelcomeViewServlet 에 questionService 추가

--- src/main/java/camp/woowa/jspcafe/servlets/view/WelcomeViewServlet.java
+++ src/main/java/camp/woowa/jspcafe/servlets/view/WelcomeViewServlet.java
@@ -1,5 +1,7 @@
 package camp.woowa.jspcafe.servlets.view;
 
+import camp.woowa.jspcafe.services.QuestionService;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -10,9 +12,19 @@ import java.io.IOException;
 
 @WebServlet(value = "/")
 public class WelcomeViewServlet extends HttpServlet {
+    private QuestionService questionService;
+
+    @Override
+    public void init() throws ServletException {
+        ServletContext sc = getServletContext();
+
+        questionService = (QuestionService) sc.getAttribute("questionService");
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
+            req.setAttribute("questions", questionService.findAll());
             req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
         } catch (ServletException | IOException e) {
             log("Error", e);
