4755ab9bd7993235c839871ce91f542098f2932a
huiseung <whtjddn75@gmail.com>
2024-07-23 12:29:14 +0900

    [feature] 사용자 회원가입 api 서블릿

---
+++ src/main/java/org/example/servlet/api/UserApi.java
@@ -0,0 +1,29 @@
+package org.example.servlet.api;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.time.LocalDateTime;
+import org.example.domain.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+@WebServlet("/api/users")
+public class UserApi extends HttpServlet {
+    private final Logger log = LoggerFactory.getLogger(UserApi.class);
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.setCharacterEncoding("UTF-8");
+        String email = request.getParameter("email");
+        String nickname = request.getParameter("nickname");
+        String password = request.getParameter("password");
+
+        User user = new User(email, nickname, password, LocalDateTime.now());
+        log.debug(user.toString());
+        response.sendRedirect("/users");
+    }
+}
