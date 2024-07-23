21a228d5b63238b609587e2ed0f164c12c62c4fd
june-777 <wlwhswnsrl96@gmail.com>
2024-07-23 19:36:28 +0900

    fix: Response 객체 매핑 로직 수정

--- src/main/java/woowa/camp/jspcafe/service/dto/UserResponse.java
+++ src/main/java/woowa/camp/jspcafe/service/dto/UserResponse.java
@@ -5,7 +5,7 @@ import woowa.camp.jspcafe.domain.User;
 public record UserResponse(Long id, String userId, String name, String email) {
 
     public static UserResponse of(User user) {
-        return new UserResponse(user.getId(), user.getUserId(), user.getName(), user.getName());
+        return new UserResponse(user.getId(), user.getUserId(), user.getName(), user.getEmail());
     }
 
 }
