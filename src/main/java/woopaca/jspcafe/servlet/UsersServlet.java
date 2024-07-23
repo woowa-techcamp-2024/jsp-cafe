ec691e555e293780e826a71ee94466afecdae7ea
jcw1031 <jcw001031@gmail.com>
2024-07-23 17:03:20 +0900

    feature: 사용자 목록 조회 서블릿 구현
    - UsersServlet doGet() 오버라이딩
    - 멤버 리스트 attribute에 추가 후 forward()

--- src/main/java/woopaca/jspcafe/servlet/UsersServlet.java
+++ src/main/java/woopaca/jspcafe/servlet/UsersServlet.java
@@ -1,5 +1,6 @@
 package woopaca.jspcafe.servlet;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -8,9 +9,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import woopaca.jspcafe.resolver.RequestParametersResolver;
 import woopaca.jspcafe.service.UserService;
+import woopaca.jspcafe.servlet.dto.MembersResponse;
 import woopaca.jspcafe.servlet.dto.SignUpRequest;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/users")
 public class UsersServlet extends HttpServlet {
@@ -34,5 +37,14 @@ public class UsersServlet extends HttpServlet {
             log.error(e.getMessage(), e);
             response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "서버 오류");
         }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<MembersResponse> members = userService.getAllMembers();
+        request.setAttribute("members", members);
+        request.setAttribute("membersCount", members.size());
+        request.getRequestDispatcher("/user/list.jsp")
+                .forward(request, response);
     }
 }
