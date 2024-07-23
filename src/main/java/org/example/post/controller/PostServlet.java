9f9a2c1ccbe10062ad80150154c341619ba32263
bellringstar <bellringstar@gmail.com>
2024-07-23 15:08:52 +0900

    feat: PostServlet
    post를 POST 요청을 받아 생성하는 서블릿

---
+++ src/main/java/org/example/post/controller/PostServlet.java
@@ -0,0 +1,38 @@
+package org.example.post.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import org.example.post.model.dao.Post;
+import org.example.post.service.PostService;
+
+@WebServlet("/questions")
+public class PostServlet extends HttpServlet {
+
+    private PostService postService;
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            String writer = req.getParameter("writer");
+            String title = req.getParameter("title");
+            String contents = req.getParameter("contents");
+            Post post = Post.create(writer, title, contents);
+            postService.create(post);
+            resp.sendRedirect("/index.html");
+        } catch (SQLException e) {
+
+        }
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        postService = new PostService();
+    }
+}
+
