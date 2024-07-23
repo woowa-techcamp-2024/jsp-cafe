3517640b618cac82bbed8bf6f8d5e0e3c809e2d0
KoKimSS <seungsu8848@ajou.ac.kr>
2024-07-23 15:04:36 +0900

    feat : 유저 관련 정보 서블릿 추가

---
+++ src/main/java/org/example/jspcafe/user/controller/UserServlet.java
@@ -0,0 +1,38 @@
+package org.example.jspcafe.user.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.jspcafe.user.service.UserService;
+
+import java.io.IOException;
+
+@WebServlet(name = "UserController", value = "/users/*")
+public class UserServlet extends HttpServlet {
+
+    private UserService userService;
+
+    public void init(){
+        this.userService = UserService.userService;
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws IOException, ServletException {
+        String pathInfo = req.getPathInfo(); // /123 형태로 반환됨
+
+        if (pathInfo == null && pathInfo.length() <= 1) {
+            res.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid user ID");
+            return;
+        }
+
+        Long id = Long.valueOf(pathInfo.substring(1)); // 앞의 '/' 제거
+        // userId를 사용하여 필요한 로직 수행
+
+        req.setAttribute("user", userService.findById(id));
+        req.getRequestDispatcher("/user/profile.jsp").forward(req, res);
+    }
+}
