692640e21709fc00fd1a9407733eb4233453f52d
HiiWee <wpdlzhf159@gmail.com>
2024-07-23 17:09:15 +0900

    feat: static 요소 처리하는 Filter 추가
    - static한 파일들이 webapp/static/ 으로 옮겼으므로 필터단에서 처리가 필요함

---
+++ src/main/java/com/wootecam/jspcafe/filter/StaticResourceFilter.java
@@ -0,0 +1,25 @@
+package com.wootecam.jspcafe.filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@WebFilter(filterName = "staticResourceFilter", urlPatterns = {"*.css", "*.js", "*.eot", "*.svg", "*.ttf", "*.woff",
+        "*.woff2", "*.png"})
+public class StaticResourceFilter implements Filter {
+
+    @Override
+    public void doFilter(final ServletRequest servletRequest, final ServletResponse servletResponse,
+                         final FilterChain filterChain) throws ServletException, IOException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+        System.out.println(request.getRequestURI());
+        request.getRequestDispatcher("/static" + request.getRequestURI())
+                .forward(servletRequest, servletResponse);
+    }
+}
