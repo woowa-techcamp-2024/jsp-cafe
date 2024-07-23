99993a575d380af54578c4f84184886c3e3e3f96
kimhyun5u <22kimhyun5u@gmail.com>
2024-07-23 13:58:40 +0900

    feat: UserViewServlet 추가

---
+++ src/main/java/camp/woowa/jspcafe/servlets/view/UserViewServlet.java
@@ -0,0 +1,29 @@
+package camp.woowa.jspcafe.servlets.view;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/user/*"})
+public class UserViewServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String pathInfo = req.getPathInfo();
+        try {
+            if ("/form".equalsIgnoreCase(pathInfo)) {
+                req.getRequestDispatcher("/WEB-INF/jsp/user/form.jsp").forward(req, resp);
+            } else if ("/list".equalsIgnoreCase(pathInfo)) {
+                req.getRequestDispatcher("/WEB-INF/jsp/user/list.jsp").forward(req, resp);
+            } else {
+                resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+            }
+        } catch (ServletException | IOException e) {
+            log("Failed to forward request", e);
+        }
+
+    }
+}
