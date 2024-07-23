4758a5f23a59f4bc1f25fcf216adbb9bc4f0f88d
hseong3243 <hseong3243@gmail.com>
2024-07-23 23:16:14 +0900

    feat: 질문 서블릿 구현
    서블릿의 테스트를 위하여 서블릿 요청, 응답 인터페이스의 스텁 객체를 구현함.

---
+++ src/test/java/com/woowa/support/StubHttpSession.java
@@ -0,0 +1,72 @@
+package com.woowa.support;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpSession;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+public class StubHttpSession implements HttpSession {
+
+    private final Map<String, Object> attributes = new HashMap<String, Object>();
+
+    @Override
+    public long getCreationTime() {
+        return 0;
+    }
+
+    @Override
+    public String getId() {
+        return "";
+    }
+
+    @Override
+    public long getLastAccessedTime() {
+        return 0;
+    }
+
+    @Override
+    public ServletContext getServletContext() {
+        return null;
+    }
+
+    @Override
+    public void setMaxInactiveInterval(int interval) {
+
+    }
+
+    @Override
+    public int getMaxInactiveInterval() {
+        return 0;
+    }
+
+    @Override
+    public Object getAttribute(String name) {
+        return attributes.get(name);
+    }
+
+    @Override
+    public Enumeration<String> getAttributeNames() {
+        return null;
+    }
+
+    @Override
+    public void setAttribute(String name, Object value) {
+        attributes.put(name, value);
+    }
+
+    @Override
+    public void removeAttribute(String name) {
+
+    }
+
+    @Override
+    public void invalidate() {
+
+    }
+
+    @Override
+    public boolean isNew() {
+        return false;
+    }
+}
