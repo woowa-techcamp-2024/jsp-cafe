928bd31c5da87092ec4ebc74f537e4c8f9145632
miiiinju1 <gms07073@ynu.ac.kr>
2024-07-23 22:10:00 +0900

    feat: page와 size를 기반으로 페이징하여 가져오는 getPosts 메서드 추가

--- src/main/java/org/example/jspcafe/post/service/PostService.java
+++ src/main/java/org/example/jspcafe/post/service/PostService.java
@@ -5,13 +5,24 @@ import org.example.jspcafe.post.model.Post;
 import org.example.jspcafe.post.repository.InMemoryPostRepository;
 import org.example.jspcafe.post.repository.PostRepository;
 import org.example.jspcafe.post.request.PostCreateRequest;
+import org.example.jspcafe.post.response.PostListResponse;
+import org.example.jspcafe.post.response.PostResponse;
+import org.example.jspcafe.user.model.Nickname;
+import org.example.jspcafe.user.model.User;
+import org.example.jspcafe.user.repository.InMemoryUserRepository;
+import org.example.jspcafe.user.repository.UserRepository;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
 public class PostService {
 
     private final PostRepository postRepository;
+    private final UserRepository userRepository;
 
     public void createPost(PostCreateRequest request) {
         final Long userId = request.userId();
@@ -23,8 +34,35 @@ public class PostService {
         postRepository.save(post);
     }
 
+    public PostListResponse getPosts(int page, int size) {
+        final List<Post> posts = postRepository.findAll(page, size);
+        final Set<Long> collectUserId = posts.stream()
+                .map(post -> post.getUserId())
+                .collect(Collectors.toUnmodifiableSet());
 
-    public PostService(InMemoryPostRepository postRepository) {
+        final Map<Long, Nickname> nicknameMap = userRepository.findAllById(collectUserId).stream()
+                .collect(Collectors.toUnmodifiableMap(User::getUserId, User::getNickname));
+
+        final List<PostResponse> postList = posts.stream()
+                .map(post -> new PostResponse(
+                        post.getPostId(),
+                        nicknameMap.get(post.getUserId()).getValue(),
+                        post.getTitle().getValue(),
+                        post.getContent().getValue(),
+                        post.getCreatedAt())
+                ).toList();
+
+        final int totalElements = postRepository.count();
+
+        return PostListResponse.of(totalElements, postList);
+    }
+
+
+    public PostService(
+            InMemoryPostRepository postRepository,
+            InMemoryUserRepository userRepository
+    ) {
         this.postRepository = postRepository;
+        this.userRepository = userRepository;
     }
 }
