9d916828ce99cb96583f8e5c05d55f9b6e215a97
HiiWee <wpdlzhf159@gmail.com>
2024-07-23 13:58:24 +0900

    rename: SignupServlet -> SignupFormServlet
    - 회원가입 폼을 보여주기만 하기때문에 이름을 변경 함

--- src/main/java/com/wootecam/jspcafe/servlet/SignupFormServlet.java
+++ src/main/java/com/wootecam/jspcafe/servlet/SignupFormServlet.java
@@ -1,15 +1,25 @@
 package com.wootecam.jspcafe.servlet;
 
 import com.wootecam.jspcafe.model.User;
+import com.wootecam.jspcafe.repository.UserRepository;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@WebServlet(value = "/users", name = "userSignupServlet")
 public class SignupServlet extends HttpServlet {
+
+    private static final Logger log = LoggerFactory.getLogger(SignupServlet.class);
+
+    private final UserRepository userRepository;
+
+    public SignupServlet(final UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     protected void doGet(final HttpServletRequest req, final HttpServletResponse resp)
@@ -27,7 +37,9 @@ public class SignupServlet extends HttpServlet {
         String email = req.getParameter("email");
         User user = new User(userId, password, name, email);
 
+        userRepository.save(user);
+        log.info("signUpUser = {}", user);
 
+        resp.sendRedirect("/users");
     }
-
 }
