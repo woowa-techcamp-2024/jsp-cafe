41e264a8806f05c1d1dd3112e8f89528f3ab2b46
miiiinju1 <gms07073@ynu.ac.kr>
2024-07-23 13:49:14 +0900

    feat: InMemoryUserRepository는 UserRepository 인터페이스를 구현하도록 변경

--- src/main/java/org/example/jspcafe/user/repository/InMemoryUserRepository.java
+++ src/main/java/org/example/jspcafe/user/repository/InMemoryUserRepository.java
@@ -4,8 +4,18 @@ import org.example.jspcafe.Component;
 import org.example.jspcafe.InMemoryRepository;
 import org.example.jspcafe.user.model.User;
 
+import java.util.Optional;
+
 @Component
-public class InMemoryUserRepository extends InMemoryRepository<User> {
+public class InMemoryUserRepository extends InMemoryRepository<User> implements UserRepository {
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return storage.values().stream()
+                .filter(user -> user.getEmail().getValue().equals(email))
+                .findFirst();
+    }
+
     public InMemoryUserRepository() {
         super(User.class);
     }
