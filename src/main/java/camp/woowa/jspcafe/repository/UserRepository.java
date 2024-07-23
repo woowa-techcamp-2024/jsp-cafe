50c01cc0946d2419c53bbf553c51b05a3d76124f
kimhyun5u <22kimhyun5u@gmail.com>
2024-07-23 10:10:42 +0900

    feat: findAll 기능 추가

--- src/main/java/camp/woowa/jspcafe/repository/UserRepository.java
+++ src/main/java/camp/woowa/jspcafe/repository/UserRepository.java
@@ -2,8 +2,12 @@ package camp.woowa.jspcafe.repository;
 
 import camp.woowa.jspcafe.models.User;
 
+import java.util.List;
+
 public interface UserRepository {
-    void save(String userId, String password, String name, String email);
+    String save(String userId, String password, String name, String email);
 
     User findById(String userId);
+
+    List<User> findAll();
 }
