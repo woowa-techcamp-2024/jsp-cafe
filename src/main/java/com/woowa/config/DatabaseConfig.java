209e73c28b96ebbb54055ca72fbfee1406f02101
hseong3243 <hseong3243@gmail.com>
2024-07-23 23:31:37 +0900

    chore: 질문 핸들러, 데이터베이스 빈 객체 등록
    싱글톤 컨테이너를 이용하기 위해 `QuestionHandler`, `QuestionDatabase`를 빈 객체로 등록함.

--- src/main/java/com/woowa/config/DatabaseConfig.java
+++ src/main/java/com/woowa/config/DatabaseConfig.java
@@ -1,5 +1,7 @@
 package com.woowa.config;
 
+import com.woowa.database.QuestionDatabase;
+import com.woowa.database.QuestionMemoryDatabase;
 import com.woowa.database.UserDatabase;
 import com.woowa.database.UserMemoryDatabase;
 import com.woowa.framework.Bean;
@@ -9,5 +11,10 @@ public class DatabaseConfig {
     @Bean
     public UserDatabase userDatabase() {
         return new UserMemoryDatabase();
+    }
+
+    @Bean
+    public QuestionDatabase questionDatabase() {
+        return new QuestionMemoryDatabase();
     }
 }
--- src/main/java/com/woowa/config/HandlerConfig.java
+++ src/main/java/com/woowa/config/HandlerConfig.java
@@ -1,7 +1,9 @@
 package com.woowa.config;
 
+import com.woowa.database.QuestionDatabase;
 import com.woowa.database.UserDatabase;
 import com.woowa.framework.Bean;
+import com.woowa.handler.QuestionHandler;
 import com.woowa.handler.UserHandler;
 
 public class HandlerConfig {
@@ -9,5 +11,10 @@ public class HandlerConfig {
     @Bean
     public UserHandler userHandler(UserDatabase userDatabase) {
         return new UserHandler(userDatabase);
+    }
+
+    @Bean
+    public QuestionHandler questionHandler(UserDatabase userDatabase, QuestionDatabase questionDatabase) {
+        return new QuestionHandler(userDatabase, questionDatabase);
     }
 }
