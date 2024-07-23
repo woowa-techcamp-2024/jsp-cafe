637e043c340b5d22e9e6120e29710b8afa5d29d1
kimhyun5u <22kimhyun5u@gmail.com>
2024-07-23 10:10:28 +0900

    test: findAll test 생성

--- src/test/java/repository/UserRepositoryTest.java
+++ src/test/java/repository/UserRepositoryTest.java
@@ -5,6 +5,8 @@ import camp.woowa.jspcafe.repository.InMemUserRepository;
 import camp.woowa.jspcafe.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UserRepositoryTest {
@@ -22,5 +24,22 @@ class UserRepositoryTest {
         // then
         User user = userRepository.findById(userId);
         assertEquals(user.getUserId(), userId);
+    }
+
+    @Test
+    void testFindAll() {
+        // given
+        String userId = "userId";
+        String password = "password";
+        String name = "name";
+        String email = "email";
+        UserRepository userRepository = new InMemUserRepository();
+        // when
+        userRepository.save(userId, password, name, email);
+        List<User> users = userRepository.findAll();
+
+        // then
+        assertEquals(users.size(), 1);
+        assertEquals(users.get(1).getUserId(), userId);
     }
 }
