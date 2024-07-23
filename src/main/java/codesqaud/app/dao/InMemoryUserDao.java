6f70bee1efd23fbb587004b6f0bf536de73a6957
seminchoi <choicco89@gmail.com>
2024-07-23 15:53:52 +0900

    feat: InMemoryUserDao userId 검색 기능 구현
    - userIdIndex 맵 추가
    - save 메소드에서 중복 아이디 검증 기능 구현

--- src/main/java/codesqaud/app/dao/InMemoryUserDao.java
+++ src/main/java/codesqaud/app/dao/InMemoryUserDao.java
@@ -7,23 +7,29 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class InMemoryUserDao implements UserDao {
-    private final Map<String, User> users = new ConcurrentHashMap<>();
-    
+    private static final AtomicLong ID_GENERATOR = new AtomicLong(0);
+
+    private final Map<String, Long> userIdIndex = new ConcurrentHashMap<>();
+    private final Map<Long, User> users = new ConcurrentHashMap<>();
+
     @Override
     public void save(User user) {
-        users.compute(user.getUserId(), (userId, existingUser) -> {
-            if (existingUser != null) {
+        userIdIndex.compute(user.getUserId(), (requestUserId, existingPk) -> {
+            if (existingPk != null) {
                 throw new IllegalStateException("이미 존재하는 사용자 입니다");
             }
-            return user;
+            user.setId(ID_GENERATOR.incrementAndGet());
+            users.put(user.getId(), user);
+            return user.getId();
         });
     }
 
     @Override
-    public Optional<User> findById(String userId) {
-        return Optional.empty();
+    public Optional<User> findById(Long PK) {
+        return Optional.ofNullable(users.get(PK));
     }
 
     @Override
@@ -33,5 +39,15 @@ public class InMemoryUserDao implements UserDao {
 
     @Override
     public void delete(User user) {
+    }
+
+    @Override
+    public Optional<User> findByUserId(String userId) {
+        if(!userIdIndex.containsKey(userId)) {
+            return Optional.empty();
+        }
+
+        Long id = userIdIndex.get(userId);
+        return Optional.of(users.get(id));
     }
 }
