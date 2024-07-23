3a8c27a434c8c2a7516244617c0ea3569a6ebc73
hellomatia <shuaigejp80@gmail.com>
2024-07-23 20:22:38 +0900

    feat: ServletContext에서 객체를 얻어와 의존성을 주입하는 기능 추가
    - ArticleService와 ArticleDao를 ServletContext에서 관리한다.

--- src/main/java/com/jspcafe/board/controller/ArticleController.java
+++ src/main/java/com/jspcafe/board/controller/ArticleController.java
@@ -1,5 +1,8 @@
 package com.jspcafe.board.controller;
 
+import com.jspcafe.board.service.ArticleService;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -10,6 +13,13 @@ import java.io.IOException;
 
 @WebServlet(urlPatterns = {"/", "/articles/*"})
 public class ArticleController extends HttpServlet {
+    private ArticleService articleService;
+
+    @Override
+    public void init() throws ServletException {
+        ServletContext ctx = getServletContext();
+        articleService = (ArticleService) ctx.getAttribute("articleService");
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -17,9 +27,20 @@ public class ArticleController extends HttpServlet {
         if (path == null || path.isEmpty() || path.isBlank()) {
             forward("board", req, resp);
         }
+        switch (path) {
+            case "/form" -> forward("article_form", req, resp);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     }
 
     private void forward(String fileName, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getRequestDispatcher("/WEB-INF/views/board/" + fileName + ".jsp").forward(req, resp);
+    }
+
+    private void writeArticle(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        forward("article_form", req, resp);
     }
 }
