926aae84d22c49baf9195f3424c28f66135bfe49
kimhyun5u <22kimhyun5u@gmail.com>
2024-07-23 15:03:34 +0900

    feat: DIContextListener 에 Question 주입 추가

--- src/main/java/camp/woowa/jspcafe/DIContextListener.java
+++ src/main/java/camp/woowa/jspcafe/DIContextListener.java
@@ -1,7 +1,10 @@
 package camp.woowa.jspcafe;
 
+import camp.woowa.jspcafe.repository.InMemQuestionRepository;
 import camp.woowa.jspcafe.repository.InMemUserRepository;
+import camp.woowa.jspcafe.repository.QuestionRepository;
 import camp.woowa.jspcafe.repository.UserRepository;
+import camp.woowa.jspcafe.services.QuestionService;
 import camp.woowa.jspcafe.services.UserService;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletContextEvent;
@@ -11,8 +14,15 @@ public class DIContextListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         ServletContext sc = sce.getServletContext();
+
+        // UserRepository를 생성하고 UserService에 주입
         UserRepository userRepository = new InMemUserRepository();
         UserService userService = new UserService(userRepository);
         sc.setAttribute("userService", userService);
+
+        // QuestionRepository를 생성하고 QuestionService에 주입
+        QuestionRepository questionRepository = new InMemQuestionRepository();
+        QuestionService questionService = new QuestionService(questionRepository);
+        sc.setAttribute("questionService", questionService);
     }
 }
