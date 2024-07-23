0c7c1efe5cb002fd98de9f0b5edddcf3d99824de
kimhyun5u <22kimhyun5u@gmail.com>
2024-07-23 14:41:12 +0900

    refactor: User Test 통합

--- src/test/java/models/UserTest.java
+++ src/test/java/models/UserTest.java
@@ -8,21 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UserTest {
     @Test
-    void testCreateUser() {
-        // given
-        String userId = "userId";
-        String password = "password";
-        String name = "name";
-        String email = "email";
-        // when
-        User user = new User(userId, password, name, email);
-
-        // then
-        assertEquals(user.getUserId(), userId);
-    }
-
-    @Test
-    void testGetter() {
+    void testUser() {
         // given
         String userId = "userId";
         String password = "password";
@@ -37,4 +23,5 @@ class UserTest {
         assertEquals(user.getEmail(), email);
         assertEquals(user.getUserId(), userId);
     }
+
 }
