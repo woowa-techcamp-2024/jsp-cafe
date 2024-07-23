70217b1ae9213236034ec2bd795853c5b63fb3ab
miiiinju1 <gms07073@ynu.ac.kr>
2024-07-23 14:57:39 +0900

    feat: 모든 사용자 목록을 가져올 수 있도록 findAll 메서드 추가

--- src/main/java/org/example/jspcafe/user/repository/InMemoryUserRepository.java
+++ src/main/java/org/example/jspcafe/user/repository/InMemoryUserRepository.java
@@ -4,6 +4,7 @@ import org.example.jspcafe.Component;
 import org.example.jspcafe.InMemoryRepository;
 import org.example.jspcafe.user.model.User;
 
+import java.util.List;
 import java.util.Optional;
 
 @Component
@@ -14,6 +15,12 @@ public class InMemoryUserRepository extends InMemoryRepository<User> implements 
         return storage.values().stream()
                 .filter(user -> user.getEmail().getValue().equals(email))
                 .findFirst();
+    }
+
+    @Override
+    public List<User> findAll() {
+        return storage.values().stream()
+                .toList();
     }
 
     public InMemoryUserRepository() {
--- src/main/java/org/example/jspcafe/user/repository/UserRepository.java
+++ src/main/java/org/example/jspcafe/user/repository/UserRepository.java
@@ -3,8 +3,10 @@ package org.example.jspcafe.user.repository;
 import org.example.jspcafe.Repository;
 import org.example.jspcafe.user.model.User;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends Repository<User> {
     Optional<User> findByEmail(String email);
+    List<User> findAll();
 }
