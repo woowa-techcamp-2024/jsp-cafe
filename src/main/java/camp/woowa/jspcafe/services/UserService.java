3b1d856cf74b76e7ba703e3f774bf4e6245c1464
kimhyun5u <22kimhyun5u@gmail.com>
2024-07-23 12:17:39 +0900

    feat: findById 기능 추가

--- src/main/java/camp/woowa/jspcafe/services/UserService.java
+++ src/main/java/camp/woowa/jspcafe/services/UserService.java
@@ -19,4 +19,8 @@ public class UserService {
     public List<User> findAll() {
         return userRepository.findAll();
     }
+
+    public User findById(String userId) {
+        return userRepository.findById(userId);
+    }
 }
