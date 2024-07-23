1c0bfa9f62914e5b7c16c1117ecc4c23ca63863a
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-07-23 17:37:25 +0900

    [Feat] ServletContext에서 MemberDao를 셋팅

--- src/main/java/com/hyeonuk/jspcafe/global/servlet/MyServletContextListener.java
+++ src/main/java/com/hyeonuk/jspcafe/global/servlet/MyServletContextListener.java
@@ -6,8 +6,11 @@ import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 
 public class MyServletContextListener implements ServletContextListener{
+    private MemberDao memberDao;
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         ServletContextListener.super.contextInitialized(sce);
+        memberDao = new InMemoryMemberDao();
+        sce.getServletContext().setAttribute("memberDao", memberDao);
     }
 }
