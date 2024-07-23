3633e899bbf0717c60effee8e64e984cefb8936d
bellringstar <bellringstar@gmail.com>
2024-07-23 15:35:24 +0900

    feat: PostService
    모든 포스트를 찾아 dto로 변환해 반환하는 메서드 추가

--- src/main/java/org/example/post/service/PostService.java
+++ src/main/java/org/example/post/service/PostService.java
@@ -1,6 +1,8 @@
 package org.example.post.service;
 
 import java.sql.SQLException;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.example.post.model.dao.Post;
 import org.example.post.model.dto.PostResponse;
 import org.example.post.repository.PostRepository;
@@ -11,6 +13,12 @@ public class PostService {
 
     public PostResponse create(Post post) throws SQLException {
         return PostResponse.toResponse(postRepository.save(post));
+    }
+
+    public List<PostResponse> getAll() throws SQLException {
+        return postRepository.findAll().stream()
+                .map(PostResponse::toResponse)
+                .collect(Collectors.toUnmodifiableList());
     }
 
 
