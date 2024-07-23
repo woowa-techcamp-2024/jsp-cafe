34e3d1f259c06bd5ceb342b174ac92d95a187b73
bellringstar <bellringstar@gmail.com>
2024-07-23 15:34:52 +0900

    feat: PostRepository
    모든 포스트를 찾는 메서드 추가

--- src/main/java/org/example/post/repository/PostRepository.java
+++ src/main/java/org/example/post/repository/PostRepository.java
@@ -2,7 +2,11 @@ package org.example.post.repository;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.rowset.serial.SerialException;
 import org.example.post.model.dao.Post;
 import org.example.util.DataUtil;
 import org.slf4j.Logger;
@@ -26,6 +30,27 @@ public class PostRepository {
         } catch (SQLException e) {
             logger.error("Error saving post", e);
             throw new SQLException(e);
+        }
+    }
+
+    public List<Post> findAll() throws SQLException {
+        String sql = "SELECT * FROM posts";
+        List<Post> posts = new ArrayList<>();
+        try (Connection conn = DataUtil.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+                Long id = rs.getLong("id");
+                String writer = rs.getString("writer");
+                String title = rs.getString("title");
+                String contents = rs.getString("contents");
+                Post post = Post.create(writer, title, contents);
+                posts.add(post);
+            }
+            return posts;
+        } catch (SQLException e) {
+            throw new SerialException("Database Error");
         }
     }
 }
