11713c700255bb627be58f87b9efbf7d60c76fa8
lass9436 <lass9436@gmail.com>
2024-07-25 03:19:04 +0900

    [repository] QuestionRepository 작성
    글을 CRUD 하기 위한 레포지토리를 작성하였습니다.

---
+++ src/main/java/lass9436/question/model/QuestionRepository.java
@@ -0,0 +1,15 @@
+package lass9436.question.model;
+
+import java.util.List;
+
+public interface QuestionRepository {
+	Question save(Question question); // Create or Update
+
+	Question findByQuestionSeq(long questionSeq); // Read by questionSeq
+
+	List<Question> findAll(); // Read all
+
+	void deleteByQuestionSeq(long questionSeq); // Delete by questionSeq
+
+	Question findByTitle(String title); // Read by title
+}
