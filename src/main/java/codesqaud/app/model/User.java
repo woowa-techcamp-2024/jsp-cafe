4f59f6a28cb0c6a0a165ffd57c2f78f69b00235b
seminchoi <choicco89@gmail.com>
2024-07-24 10:39:27 +0900

    feat: User Email 정규식 패턴 강화

--- src/main/java/codesqaud/app/model/User.java
+++ src/main/java/codesqaud/app/model/User.java
@@ -3,7 +3,7 @@ package codesqaud.app.model;
 import java.util.regex.Pattern;
 
 public class User {
-    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9+_.-]+@(.+)$");
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9+_.-]+@[\\w]+(\\.+)[\\w]+$");
 
     private Long id;
     private String userId;
@@ -71,7 +71,19 @@ public class User {
         return email;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
