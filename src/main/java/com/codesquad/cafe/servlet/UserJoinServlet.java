25f41d181757dce8b2b8bf6f6c347ba7060c2e46
김수현 <i960107@naver.com>
2024-07-25 07:25:23 +0900

    feat: 회원가입 서블릿
    - get 요청시 폼 응답
    - post 요청시 회원가입 후 유저 리스트 페이지로 redirect. 중복된 id로 실패시 다시 회원가입 페이지 응답

---
+++ src/main/java/com/codesquad/cafe/servlet/UserJoinServlet.java
@@ -0,0 +1,49 @@
+package com.codesquad.cafe.servlet;
+
+import com.codesquad.cafe.db.InMemoryUserRepository;
+import com.codesquad.cafe.model.User;
+import com.codesquad.cafe.model.UserJoinRequest;
+import com.codesquad.cafe.util.RequestParamModelMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@WebServlet("/users/join")
+public class UserJoinServlet extends HttpServlet {
+
+    private static final Logger log = LoggerFactory.getLogger(UserJoinServlet.class);
+
+    private InMemoryUserRepository userRepository;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        this.userRepository = (InMemoryUserRepository) getServletContext().getAttribute("userRepository");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/user_join_form.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UserJoinRequest user = RequestParamModelMapper.map(req.getParameterMap(), UserJoinRequest.class);
+        log.debug("user: {}", user);
+
+        if (userRepository.findByUsername(user.getUsername()).isPresent()) {
+            req.setAttribute("error", "중복된 이메일입니다.");
+            req.getRequestDispatcher("/WEB-INF/views/user_join_form.jsp").forward(req, resp);
+            return;
+        }
+
+        userRepository.save(user.toUser());
+        resp.sendRedirect("/users");
+    }
+}
