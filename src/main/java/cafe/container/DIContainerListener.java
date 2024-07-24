c413487387586528e9c05246cbf4530807bb1b84
minseok-oh <minseok.dev@gmail.com>
2024-07-24 09:34:22 +0900

    feat: article service와 database 의존성 주입

--- src/main/java/cafe/container/DIContainerListener.java
+++ src/main/java/cafe/container/DIContainerListener.java
@@ -1,6 +1,8 @@
 package cafe.container;
 
+import cafe.domain.db.ArticleDatabase;
 import cafe.domain.db.UserDatabase;
+import cafe.service.ArticleService;
 import cafe.service.UserService;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletContextEvent;
@@ -10,10 +12,19 @@ public class DIContainerListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         ServletContext servletContext = sce.getServletContext();
+        userInit(servletContext);
+        articleInit(servletContext);
+    }
 
+    private void userInit(ServletContext servletContext) {
         UserDatabase userDatabase = new UserDatabase();
         UserService userService = new UserService(userDatabase);
-
         servletContext.setAttribute("userService", userService);
+    }
+
+    private void articleInit(ServletContext servletContext) {
+        ArticleDatabase articleDatabase = new ArticleDatabase();
+        ArticleService articleService = new ArticleService(articleDatabase);
+        servletContext.setAttribute("articleService", articleService);
     }
 }
