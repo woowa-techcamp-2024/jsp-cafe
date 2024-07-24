9e6b688171193014ae7a045eefd0697614932a63
KyungMin Lee <hello.tidavid@gmail.com>
2024-07-24 18:09:32 +0900

    feat: MySQL Article 도메인 연결
    - `DefaultServletContextListener` 에서 MySQLConnectionManager를 등록해 주입할 수 있도록 변경했습니다.
    - sql url을 수정했습니다.

--- src/main/java/codesquad/jspcafe/servlet/DefaultServletContextListener.java
+++ src/main/java/codesquad/jspcafe/servlet/DefaultServletContextListener.java
@@ -1,17 +1,38 @@
 package codesquad.jspcafe.servlet;
 
+import codesquad.jspcafe.common.ApplicationProperties;
+import codesquad.jspcafe.common.database.MySQLConnectionManager;
+import codesquad.jspcafe.domain.article.repository.ArticleJdbcRepository;
 import codesquad.jspcafe.domain.article.service.ArticleService;
 import codesquad.jspcafe.domain.user.service.UserService;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @WebListener
 public class DefaultServletContextListener implements ServletContextListener {
 
+    private static final Logger log = LoggerFactory.getLogger(DefaultServletContextListener.class);
+
+    private final ApplicationProperties applicationProperties = new ApplicationProperties();
+    private final MySQLConnectionManager connectionManager = new MySQLConnectionManager(
+        applicationProperties);
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+        jdbcDriverInit();
         sce.getServletContext().setAttribute("userService", new UserService());
-        sce.getServletContext().setAttribute("articleService", new ArticleService());
+        sce.getServletContext().setAttribute("articleService",
+            new ArticleService(new ArticleJdbcRepository(connectionManager)));
+    }
+
+    private void jdbcDriverInit() {
+        try {
+            Class.forName(applicationProperties.getJdbcDriver());
+        } catch (ClassNotFoundException e) {
+            log.error(e.getMessage());
+        }
     }
 }
