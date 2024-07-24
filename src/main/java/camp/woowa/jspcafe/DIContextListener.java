99da070d7273a4b51866a59d386cd405bdbfcb8b
kimhyun5u <22kimhyun5u@gmail.com>
2024-07-24 21:17:19 +0900

    update: DB 의존성 mysql로 변경

--- src/main/java/camp/woowa/jspcafe/DIContextListener.java
+++ src/main/java/camp/woowa/jspcafe/DIContextListener.java
@@ -1,27 +1,37 @@
 package camp.woowa.jspcafe;
 
-import camp.woowa.jspcafe.repository.InMemQuestionRepository;
-import camp.woowa.jspcafe.repository.InMemUserRepository;
-import camp.woowa.jspcafe.repository.QuestionRepository;
-import camp.woowa.jspcafe.repository.UserRepository;
+import camp.woowa.jspcafe.db.DatabaseManager;
+import camp.woowa.jspcafe.repository.*;
 import camp.woowa.jspcafe.services.QuestionService;
 import camp.woowa.jspcafe.services.UserService;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 
+import java.sql.Connection;;
+
 public class DIContextListener implements ServletContextListener {
+    private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(DIContextListener.class);
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         ServletContext sc = sce.getServletContext();
 
+        // DB Connection
+        Connection conn = DatabaseManager.getConnection();
+
+        if (conn == null) {
+            logger.error("DB Connection Error");
+            throw new RuntimeException("DB Connection Error");
+        }
+
         // UserRepository를 생성하고 UserService에 주입
-        UserRepository userRepository = new InMemUserRepository();
+        UserRepository userRepository = new MySQLUserRepository(conn);
         UserService userService = new UserService(userRepository);
         sc.setAttribute("userService", userService);
 
         // QuestionRepository를 생성하고 QuestionService에 주입
-        QuestionRepository questionRepository = new InMemQuestionRepository();
+        QuestionRepository questionRepository = new MySQLQuestionRepository(conn);
         QuestionService questionService = new QuestionService(questionRepository);
         sc.setAttribute("questionService", questionService);
     }
