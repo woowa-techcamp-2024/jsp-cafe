7b65bbd74cebaf72a2bb11cf58c43bd9cfd6c2b1
HiiWee <wpdlzhf159@gmail.com>
2024-07-24 12:44:53 +0900

    feat: Question에 LocalDateTime 필드 추가
    - 글 작성 시간을 저장하기 위함

--- src/main/java/com/wootecam/jspcafe/model/Question.java
+++ src/main/java/com/wootecam/jspcafe/model/Question.java
@@ -1,5 +1,6 @@
 package com.wootecam.jspcafe.model;
 
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 public class Question {
@@ -8,24 +9,28 @@ public class Question {
     private final String writer;
     private final String title;
     private final String contents;
+    private final LocalDateTime createdTime;
 
-    public Question(final String writer, final String title, final String contents) {
-        validate(writer, title, contents);
+    public Question(final String writer, final String title, final String contents, final LocalDateTime createdTime) {
+        validate(writer, title, contents, createdTime);
         this.writer = writer;
         this.title = title;
         this.contents = contents;
+        this.createdTime = createdTime;
     }
 
-    private void validate(final String writer, final String title, final String contents) {
-        if (Objects.isNull(writer) || Objects.isNull(title) || Objects.isNull(contents)
+    public Question(final Long id, final String writer, final String title, final String contents,
+                    final LocalDateTime createdTime) {
+        this(writer, title, contents, createdTime);
+        this.id = id;
+    }
+
+    private void validate(final String writer, final String title, final String contents,
+                          final LocalDateTime createdTime) {
+        if (Objects.isNull(writer) || Objects.isNull(title) || Objects.isNull(contents) || Objects.isNull(createdTime)
                 || writer.isEmpty() || title.isEmpty() || contents.isEmpty()) {
             throw new IllegalArgumentException("질문 작성 시 모든 정보를 입력해야 합니다.");
         }
-    }
-
-    public Question(final Long id, final String writer, final String title, final String contents) {
-        this(writer, title, contents);
-        this.id = id;
     }
 
     public Long getId() {
@@ -44,6 +49,10 @@ public class Question {
         return contents;
     }
 
+    public LocalDateTime getCreatedTime() {
+        return createdTime;
+    }
+
     @Override
     public String toString() {
         return "Question{" +
@@ -51,6 +60,7 @@ public class Question {
                 ", writer='" + writer + '\'' +
                 ", title='" + title + '\'' +
                 ", contents='" + contents + '\'' +
+                ", createdTime=" + createdTime +
                 '}';
     }
 }
--- src/test/java/com/wootecam/jspcafe/model/QuestionTest.java
+++ src/test/java/com/wootecam/jspcafe/model/QuestionTest.java
@@ -2,6 +2,7 @@ package com.wootecam.jspcafe.model;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.Test;
 
 class QuestionTest {
@@ -9,7 +10,7 @@ class QuestionTest {
     @Test
     void 질문_폼_입력에서_하나라도_비어있다면_예외가_발생한다() {
         // expect
-        assertThatThrownBy(() -> new Question("", "안녕하세요!", "반갑습니다."))
+        assertThatThrownBy(() -> new Question("", "안녕하세요!", "반갑습니다.", LocalDateTime.now()))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("질문 작성 시 모든 정보를 입력해야 합니다.");
     }
@@ -17,7 +18,7 @@ class QuestionTest {
     @Test
     void 질문_폼_입력에서_하나라도_null이_입력되면_예외가_발생한다() {
         // expect
-        assertThatThrownBy(() -> new Question("HiiWee", "안녕하세요!", null))
+        assertThatThrownBy(() -> new Question("HiiWee", "안녕하세요!", null, LocalDateTime.now()))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("질문 작성 시 모든 정보를 입력해야 합니다.");
     }
