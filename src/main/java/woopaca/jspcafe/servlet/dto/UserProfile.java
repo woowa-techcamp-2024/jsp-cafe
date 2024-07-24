0a471ef878ef68c767b287033647c430ede1d519
jcw1031 <jcw001031@gmail.com>
2024-07-24 20:04:42 +0900

    refactor: 불필요한 필드 제거
    - UserProfile 레코드의 password 필드 제거

--- src/main/java/woopaca/jspcafe/servlet/dto/UserProfile.java
+++ src/main/java/woopaca/jspcafe/servlet/dto/UserProfile.java
@@ -4,10 +4,10 @@ import woopaca.jspcafe.model.User;
 
 import java.time.format.DateTimeFormatter;
 
-public record UserProfile(String nickname, String email, String signUpAt, String password) {
+public record UserProfile(String nickname, String email, String signUpAt) {
 
     public static UserProfile from(User user) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일에 가입");
-        return new UserProfile(user.getNickname(), user.getUsername(), user.getCreatedAt().format(formatter), user.getPassword());
+        return new UserProfile(user.getNickname(), user.getUsername(), user.getCreatedAt().format(formatter));
     }
 }
