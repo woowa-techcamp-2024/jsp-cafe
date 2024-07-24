5e02107a548cff33e7457f2ddf8c9d68a27cd60a
jcw1031 <jcw001031@gmail.com>
2024-07-24 20:07:07 +0900

    refactor: PostService 컨텍스트 등록
    - Servlet에서 PostService를 사용할 수 있도록 ServletContext에 저장
    - PostRepository 의존성 주입

--- src/main/java/woopaca/jspcafe/config/ApplicationConfiguration.java
+++ src/main/java/woopaca/jspcafe/config/ApplicationConfiguration.java
@@ -4,8 +4,11 @@ import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
+import woopaca.jspcafe.repository.PostMemoryRepository;
+import woopaca.jspcafe.repository.PostRepository;
 import woopaca.jspcafe.repository.UserMemoryRepository;
 import woopaca.jspcafe.repository.UserRepository;
+import woopaca.jspcafe.service.PostService;
 import woopaca.jspcafe.service.UserService;
 
 @WebListener
@@ -15,6 +18,7 @@ public class ApplicationConfiguration implements ServletContextListener {
     public void contextInitialized(ServletContextEvent sce) {
         ServletContext servletContext = sce.getServletContext();
         servletContext.setAttribute("userService", userService());
+        servletContext.setAttribute("postService", postService());
     }
 
     private UserService userService() {
@@ -23,5 +27,13 @@ public class ApplicationConfiguration implements ServletContextListener {
 
     private UserRepository userRepository() {
         return new UserMemoryRepository();
+    }
+
+    private PostService postService() {
+        return new PostService(postRepository(), userRepository());
+    }
+
+    private PostRepository postRepository() {
+        return new PostMemoryRepository();
     }
 }
