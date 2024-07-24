4775cec5591beef9b0046f2b79acc57031dd2f1b
kimhyun5u <22kimhyun5u@gmail.com>
2024-07-24 12:59:06 +0900

    feat: 유저 검증 기능 추가

--- src/main/java/camp/woowa/jspcafe/servlets/QuestionsServlet.java
+++ src/main/java/camp/woowa/jspcafe/servlets/QuestionsServlet.java
@@ -1,6 +1,9 @@
 package camp.woowa.jspcafe.servlets;
 
+import camp.woowa.jspcafe.exception.CustomException;
+import camp.woowa.jspcafe.exception.HttpStatus;
 import camp.woowa.jspcafe.services.QuestionService;
+import camp.woowa.jspcafe.services.UserService;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -13,11 +16,14 @@ import java.io.IOException;
 @WebServlet(value = "/questions")
 public class QuestionsServlet extends HttpServlet {
     private QuestionService questionService;
+    private UserService userService;
+
     @Override
     public void init() throws ServletException {
         ServletContext sc = getServletContext();
 
         questionService = (QuestionService) sc.getAttribute("questionService");
+        userService = (UserService) sc.getAttribute("userService");
     }
 
     @Override
@@ -26,12 +32,21 @@ public class QuestionsServlet extends HttpServlet {
         String content = req.getParameter("content");
         String writer = req.getParameter("writer");
 
+
+        isExistedByUserIdOrThrow(writer); // Check if the user exists
+
         questionService.save(title, content, writer);
+
 
         try {
             resp.sendRedirect("/");
         } catch (IOException e) {
             log("Redirect Error", e);
         }
+    }
+
+    private void isExistedByUserIdOrThrow(String writer) {
+        if (!userService.isExistedByUserId(writer))
+            throw new CustomException(HttpStatus.USER_NOT_FOUND);
     }
 }
