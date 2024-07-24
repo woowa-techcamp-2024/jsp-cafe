adcbb21edf41ce25f0c85a284f612553f3769623
miiiinju1 <gms07073@ynu.ac.kr>
2024-07-24 15:12:46 +0900

    feat: deleteAllInBatch 인터페이스에 추가

--- src/main/java/org/example/jspcafe/user/repository/UserRepository.java
+++ src/main/java/org/example/jspcafe/user/repository/UserRepository.java
@@ -6,11 +6,12 @@ import org.example.jspcafe.user.model.User;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 public interface UserRepository extends Repository<User> {
     Optional<User> findByEmail(String email);
     Optional<User> findByNickname(String nickname);
     List<User> findAll();
     List<User> findAllById(Collection<Long> collectUserId);
+
+    void deleteAllInBatch();
 }
