ad5f0f84f008a15af6efae71d64455aadf19e0ea
huiseung <whtjddn75@gmail.com>
2024-07-24 10:33:40 +0900

    [fix] driver load 실패 오류 발생/해결 - driver class load 코드 작성

--- src/main/java/org/example/data/DatabaseConnectionManager.java
+++ src/main/java/org/example/data/DatabaseConnectionManager.java
@@ -6,9 +6,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnectionManager {
+
     private static String url = "jdbc:mysql://localhost:3306/mydb";
     private static String user = "user";
     private static String password = "userpassword";
+
+    static {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(url, user, password);
