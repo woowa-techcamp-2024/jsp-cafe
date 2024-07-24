acf88feb809a1ca8ad237df4941e5eff203dd1a9
CodingLuizy <luizy991212@gmail.com>
2024-07-24 11:23:18 +0900

    refactor: User record -> class
    - jstl 라이브러리를 사용하기 위해 EL 문법에 따라 getter가 필요해짐

--- src/main/java/cafe/users/User.java
+++ src/main/java/cafe/users/User.java
@@ -1,11 +1,16 @@
 package cafe.users;
 
-public record User(Long id, String userId, String username, String password) {
+public class User {
+    private final Long id;
+    private final String userId;
+    private final String username;
+    private final String password;
+
     public User(String userId, String username, String password) {
         this(null, userId, username, password);
     }
 
-    public User {
+    public User(Long id, String userId, String username, String password) {
         if (userId == null || userId.isBlank()) {
             throw new IllegalArgumentException("Email must not be null or empty");
         }
@@ -15,9 +20,29 @@ public record User(Long id, String userId, String username, String password) {
         if (password == null || password.isBlank()) {
             throw new IllegalArgumentException("Password must not be null or empty");
         }
+        this.id = id;
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
     }
 
     public User withId(Long id) {
         return new User(id, this.userId, this.username, this.password);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
     }
 }
