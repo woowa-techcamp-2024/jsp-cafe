b22b97e5f9420d64b96864f39082e31f2c1a71e6
김수현 <i960107@naver.com>
2024-07-25 07:27:07 +0900

    feat: 유저 프로필 조회 서블릿 추가
    - path varaible로 전달된 id에 해당하는 유저가 없으면 404 페이지 반환
    - 유저 프로필 페이지 응답

---
+++ src/main/java/com/codesquad/cafe/servlet/UserServlet.java
@@ -0,0 +1,41 @@
+package com.codesquad.cafe.servlet;
+
+import com.codesquad.cafe.db.InMemoryUserRepository;
+import com.codesquad.cafe.model.User;
+import com.codesquad.cafe.util.RequestParamModelMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@WebServlet("/users/*")
+public class UserServlet extends HttpServlet {
+
+    private static final Logger log = LoggerFactory.getLogger(UserServlet.class);
+
+    private InMemoryUserRepository userRepository;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        this.userRepository = (InMemoryUserRepository) getServletContext().getAttribute("userRepository");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Long id = Long.parseLong(req.getPathInfo().substring(1));
+        log.warn("pathInfo: {}", id);
+        Optional<User> user = userRepository.findById(id);
+        if (user.isEmpty()) {
+            req.getRequestDispatcher("/static/error/404.html").forward(req, resp);
+            return;
+        }
+        req.setAttribute("user", user.get());
+        req.getRequestDispatcher("/WEB-INF/views/user_profile.jsp").forward(req, resp);
+    }
+}
