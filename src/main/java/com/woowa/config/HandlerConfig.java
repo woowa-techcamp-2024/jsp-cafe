befbe8d0281ab95497d4ce730bae83a8a99b0740
hseong3243 <hseong3243@gmail.com>
2024-07-24 09:16:35 +0900

    chore: 로그인 핸들러 빈 객체 등록
    싱글톤 컨테이너에 등록하고 사용하기 위하여 핸들러 설정 파일에 추가함.

--- src/main/java/com/woowa/config/HandlerConfig.java
+++ src/main/java/com/woowa/config/HandlerConfig.java
@@ -2,7 +2,9 @@ package com.woowa.config;
 
 import com.woowa.database.QuestionDatabase;
 import com.woowa.database.UserDatabase;
+import com.woowa.database.UserMemoryDatabase;
 import com.woowa.framework.Bean;
+import com.woowa.handler.LoginHandler;
 import com.woowa.handler.QuestionHandler;
 import com.woowa.handler.UserHandler;
 
@@ -16,5 +18,10 @@ public class HandlerConfig {
     @Bean
     public QuestionHandler questionHandler(UserDatabase userDatabase, QuestionDatabase questionDatabase) {
         return new QuestionHandler(userDatabase, questionDatabase);
+    }
+
+    @Bean
+    public LoginHandler loginHandler(UserDatabase userDatabase) {
+        return new LoginHandler(userDatabase);
     }
 }
