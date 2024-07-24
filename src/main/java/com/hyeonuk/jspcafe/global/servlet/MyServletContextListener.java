27773a7a7a0c86d44de88d1809671c57a048715d
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-07-24 12:10:12 +0900

    [Fix] ServletContext에 passwordEncoder를 설정

--- src/main/java/com/hyeonuk/jspcafe/global/servlet/MyServletContextListener.java
+++ src/main/java/com/hyeonuk/jspcafe/global/servlet/MyServletContextListener.java
@@ -1,5 +1,7 @@
 package com.hyeonuk.jspcafe.global.servlet;
 
+import com.hyeonuk.jspcafe.global.utils.BcryptPasswordEncoder;
+import com.hyeonuk.jspcafe.global.utils.PasswordEncoder;
 import com.hyeonuk.jspcafe.member.dao.InMemoryMemberDao;
 import com.hyeonuk.jspcafe.member.dao.MemberDao;
 import jakarta.servlet.ServletContextEvent;
@@ -7,10 +9,14 @@ import jakarta.servlet.ServletContextListener;
 
 public class MyServletContextListener implements ServletContextListener{
     private MemberDao memberDao;
+    private PasswordEncoder passwordEncoder;
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         ServletContextListener.super.contextInitialized(sce);
         memberDao = new InMemoryMemberDao();
+        passwordEncoder = new BcryptPasswordEncoder();
         sce.getServletContext().setAttribute("memberDao", memberDao);
+        sce.getServletContext().setAttribute("passwordEncoder", passwordEncoder);
+
     }
 }
