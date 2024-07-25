920192c8d02d797e63d978252fc9ed84a597814a
minnim1010 <pms1010123@daum.net>
2024-07-25 14:43:35 +0900

    refactor: 데이터베이스 커넥션 관리를 HikariCP로 설정
    성능 향상을 위해 HikariCP 의존성 추가 및 커넥션 풀 사용하도록 수정

--- src/main/java/org/example/cafe/infrastructure/database/DbConnector.java
+++ src/main/java/org/example/cafe/infrastructure/database/DbConnector.java
@@ -2,29 +2,26 @@ package org.example.cafe.infrastructure.database;
 
 import static org.example.cafe.utils.LoggerFactory.getLogger;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import javax.sql.DataSource;
 import org.example.cafe.common.error.CafeException;
 import org.slf4j.Logger;
 
 public class DbConnector {
 
     private static final Logger log = getLogger(DbConnector.class);
-
-    private static final String DATASOURCE_URL = "jdbc:mysql://localhost:3306/cafe";
-    private static final String DATASOURCE_USER = "test";
-    private static final String DATASOURCE_PASSWORD = "qlalfqjsgh";
     private static final String HEALTH_CHECK = "SELECT 1";
+    private HikariConfig config;
+    private DataSource dataSource;
 
     public DbConnector init() {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new CafeException("Cannot load MySQL JDBC Driver", e);
-        }
+        this.config = new HikariConfig("/setting.properties");
+        this.dataSource = new HikariDataSource(config);
 
         return healthCheck();
     }
@@ -33,7 +30,7 @@ public class DbConnector {
         try (Statement statement = getConnection().createStatement();
              ResultSet resultSet = statement.executeQuery(HEALTH_CHECK)) {
             if (resultSet.next() && resultSet.getInt(1) == 1) {
-                log.info(DATASOURCE_URL + " connected.");
+                log.info(config.getJdbcUrl() + " connected.");
                 return this;
             }
             return null;
@@ -43,6 +40,6 @@ public class DbConnector {
     }
 
     public Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(DATASOURCE_URL, DATASOURCE_USER, DATASOURCE_PASSWORD);
+        return this.dataSource.getConnection();
     }
 }
