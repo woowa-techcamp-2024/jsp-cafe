5ba503e2dcf4352da4ef857e3bf3108f751b3a59
june-777 <wlwhswnsrl96@gmail.com>
2024-07-25 14:37:44 +0900

    feat: UserRepository 인터페이스 update 시그니쳐 추가
    사용자의 갱신할 수 있는 정보가 바뀔 수 있으므로, DTO 객체를 사용함

--- src/main/java/woowa/camp/jspcafe/repository/UserRepository.java
+++ src/main/java/woowa/camp/jspcafe/repository/UserRepository.java
@@ -3,6 +3,7 @@ package woowa.camp.jspcafe.repository;
 import java.util.List;
 import java.util.Optional;
 import woowa.camp.jspcafe.domain.User;
+import woowa.camp.jspcafe.repository.dto.UserUpdateRequest;
 
 public interface UserRepository {
 
@@ -12,4 +13,5 @@ public interface UserRepository {
 
     Optional<User> findById(Long id);
 
+    void update(Long userId, UserUpdateRequest userUpdateRequest);
 }
