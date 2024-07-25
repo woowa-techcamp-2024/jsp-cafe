212d0a9d115f0e9eb3542ea9cffb9fe98bbadc7e
김수현 <i960107@naver.com>
2024-07-26 08:54:43 +0900

    feat: 유저 정보 수정 기능 추가
    - get 요청시 폼 반환
    - post 요청시 유저 정보 수정

---
+++ src/main/java/com/codesquad/cafe/model/UserEditRequest.java
@@ -0,0 +1,40 @@
+package com.codesquad.cafe.model;
+
+public class UserEditRequest {
+
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String confirmPassword;
+
+    private String name;
+
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
