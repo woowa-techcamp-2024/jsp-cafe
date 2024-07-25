d75dbc1661b2b2d73b3c6a0a8d79f81ed357aded
huiseung <whtjddn75@gmail.com>
2024-07-25 13:26:42 +0900

    [feature] 회원가입시 제약 조건 확인 - 데이터베이스 스키마로 확인하던걸 애플리케이션에서 확인 - 입력 길이 - 이메일 중복 확인
    - 목적
      - 데이터베이스 커넥션 활용 전 오류 확인
      - 에러페이지 응답

--- src/main/java/org/example/servlet/api/UserRegisterApi.java
+++ src/main/java/org/example/servlet/api/UserRegisterApi.java
@@ -28,15 +28,43 @@ public class UserRegisterApi extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         request.setCharacterEncoding("UTF-8");
         String email = request.getParameter("email");
         String nickname = request.getParameter("nickname");
         String password = request.getParameter("password");
 
+        if(isInvalidInput(request, response, email, nickname, password)) return;
+        User findUser = userDataHandler.findByEmail(email);
+        if(isDuplicateEmail(request, response, findUser));
         User user = new User(email, nickname, password, LocalDateTime.now());
         User savedUser = userDataHandler.insert(user);
         log.debug("[UserRegisterApi]" + savedUser.toString());
-        response.sendRedirect("/users");
+        response.sendRedirect("/login");
     }
+
+
+    private boolean isInvalidInput(HttpServletRequest request, HttpServletResponse response, String email,  String nickname, String password) throws ServletException, IOException {
+        if(!(email.length() < 255 && nickname.length() < 100 && password.length() < 255)){
+            request.setAttribute("status_code", HttpServletResponse.SC_BAD_REQUEST);
+            request.setAttribute("message", String.format("이메일 %d 글자, 닉네임 %d 글자, 비밀번호 %d 글자 보다 작은 지 확인 바랍니다", 255, 100, 255));
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            request.getRequestDispatcher("/error/error.jsp").forward(request, response);
+            return true;
+        }
+        return false;
+    }
+
+
+    private boolean isDuplicateEmail(HttpServletRequest request, HttpServletResponse response, User user) throws ServletException, IOException {
+        if(user != null){
+            request.setAttribute("status_code", HttpServletResponse.SC_BAD_REQUEST);
+            request.setAttribute("message", "해당 이메일에 사용자가 이미 있습니다.");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            request.getRequestDispatcher("/error/error.jsp").forward(request, response);
+            return true;
+        }
+        return false;
+    }
+
 }
