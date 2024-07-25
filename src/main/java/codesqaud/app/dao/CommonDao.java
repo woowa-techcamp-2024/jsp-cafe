3712f0bf699cc0e299f038b70f1059ce677acc4e
seminchoi <choicco89@gmail.com>
2024-07-25 11:29:31 +0900

    feat: Dao 인터페이스 save, update 메소드 분리

--- src/main/java/codesqaud/app/dao/CommonDao.java
+++ src/main/java/codesqaud/app/dao/CommonDao.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 public interface CommonDao <TARGET, PK> {
     void save(TARGET TARGET);
+    void update(TARGET TARGET);
     Optional<TARGET> findById(PK PK);
     List<TARGET> findAll();
     void delete(TARGET TARGET);
