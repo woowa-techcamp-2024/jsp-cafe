920ae22ea985f766a47dda7abf57d671161f07d1
donghar <donghar@naver.com>
2024-07-25 13:19:25 +0900

    [Feat] docker로 패키징
    로컬에서는 application.properties 설정 사용
    Docker로 배포할 때는 환경변수를 읽어서 DB와 연결

---
+++ src/main/resources/db/init/init.sql
@@ -0,0 +1,19 @@
+CREATE SCHEMA IF NOT EXISTS jsp_cafe;
+
+CREATE TABLE IF NOT EXISTS jsp_cafe.users
+(
+    id       BIGINT AUTO_INCREMENT PRIMARY KEY, -- Auto-incrementing ID, primary key
+    userId   VARCHAR(50)  NOT NULL UNIQUE,      -- Unique user ID
+    password VARCHAR(255) NOT NULL,             -- Password field
+    name     VARCHAR(100) NOT NULL,             -- User's name
+    email    VARCHAR(100) NOT NULL UNIQUE       -- User's email, must be unique
+    );
+
+CREATE TABLE IF NOT EXISTS jsp_cafe.articles
+(
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title      VARCHAR(255) NOT NULL,
+    writer     VARCHAR(255) NOT NULL,
+    content    TEXT         NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
