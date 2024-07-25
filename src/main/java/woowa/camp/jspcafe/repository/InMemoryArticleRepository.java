c72378006233adec79cfd575c8265f511f738d90
june-777 <wlwhswnsrl96@gmail.com>
2024-07-25 12:42:12 +0900

    feat: 인메모리 저장소에 게시글을 저장하는 기능

---
+++ src/main/java/woowa/camp/jspcafe/repository/InMemoryArticleRepository.java
@@ -0,0 +1,24 @@
+package woowa.camp.jspcafe.repository;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+import woowa.camp.jspcafe.domain.Article;
+
+public class InMemoryArticleRepository implements ArticleRepository {
+
+    private final Map<Long, Article> articles = new ConcurrentHashMap<>();
+    private final AtomicLong id = new AtomicLong();
+
+    @Override
+    public Long save(Article article) {
+        long currentId = this.id.incrementAndGet();
+        article.setId(currentId);
+        articles.put(currentId, article);
+        return currentId;
+    }
+
+}
