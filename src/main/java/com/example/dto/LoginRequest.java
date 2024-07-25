6c9cdb51a73c92e845cefc2755aa810bc428c2a3
kariskan <zkfzpf56@naver.com>
2024-07-25 11:21:04 +0900

    feat: 필요한 dto 추가

---
+++ src/main/java/com/example/dto/LoginRequest.java
@@ -0,0 +1,7 @@
+package com.example.dto;
+
+public record LoginRequest(
+	String id,
+	String password
+) {
+}
---
+++ src/main/java/com/example/dto/SaveArticleRequest.java
@@ -0,0 +1,7 @@
+package com.example.dto;
+
+public record SaveArticleRequest(
+	String title,
+	String contents
+) {
+}
