a6a3f589484c00fcc91c914753675137ede6f5e4
KoKimSS <seungsu8848@ajou.ac.kr>
2024-07-25 10:49:30 +0900

    feat : 에러 핸들링 필터 구현

---
+++ src/main/java/org/example/jspcafe/filter/ErrorHandlingFilter.java
@@ -0,0 +1,41 @@
+package org.example.jspcafe.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.io.IOException;
+
+@WebFilter(filterName = "ErrorHandlingFilter", urlPatterns = {"/*"})
+public class ErrorHandlingFilter implements Filter {
+    private String paramName;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // 필터 초기화 시 초기화 매개변수 가져오기
+        paramName = filterConfig.getInitParameter("paramName");
+        System.out.println("Initialized with paramName = " + paramName);
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+
+        try {
+            // 다음 필터 또는 서블릿으로 요청 전달
+            chain.doFilter(request, response);
+        } catch (Exception ex) {
+            // 예외 처리
+            // 예외가 발생했을 때 원래 요청의 URL로 포워딩
+            String originalUrl = httpRequest.getRequestURI();
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/error.jsp");
+            dispatcher.forward(request, response);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
