026eaa65e122be63e2de0bdd70d7d20448112d2f
jcw1031 <jcw001031@gmail.com>
2024-07-25 12:38:44 +0900

    test: UserService 테스트
    - getAllMembers() 테스트
    - getUserProfile() 테스트

--- src/test/java/woopaca/jspcafe/service/UserServiceTest.java
+++ src/test/java/woopaca/jspcafe/service/UserServiceTest.java
@@ -4,8 +4,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import woopaca.jspcafe.fixture.TestRepositoryFixture;
+import woopaca.jspcafe.servlet.dto.MembersResponse;
 import woopaca.jspcafe.servlet.dto.SignUpRequest;
+import woopaca.jspcafe.servlet.dto.UserProfile;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -74,6 +79,37 @@ class UserServiceTest {
                         .isInstanceOf(IllegalArgumentException.class)
                         .hasMessage("[ERROR] 이미 사용 중인 닉네임입니다.");
             }
+        }
+    }
+
+    @Nested
+    class get_all_members_메서드는 {
+
+        @Test
+        void 모든_회원을_조회한다() {
+            List<MembersResponse> members = userService.getAllMembers();
+            assertThat(members).isNotNull();
+            assertThat(members).hasSize(3);
+        }
+    }
+
+    @Nested
+    class get_user_profile_메서드는 {
+
+        @Test
+        void 사용자_프로필을_조회한다() {
+            String userId = "111";
+            UserProfile userProfile = userService.getUserProfile(userId);
+            assertThat(userProfile).isNotNull();
+            assertThat(userProfile.nickname()).isEqualTo("test");
+        }
+
+        @Test
+        void 사용자_프로필을_조회할_수_없는_경우_예외가_발생한다() {
+            String userId = "-1";
+            assertThatThrownBy(() -> userService.getUserProfile(userId))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("[ERROR] 사용자를 찾을 수 없습니다.");
         }
     }
 }
