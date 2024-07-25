30bd9de762296e2f8d6a6edb3948fee99bdcce65
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-07-26 03:10:54 +0900

    [Fix] System 출력 코드 삭제

--- src/main/java/com/hyeonuk/jspcafe/global/servlet/filter/SessionFilter.java
+++ src/main/java/com/hyeonuk/jspcafe/global/servlet/filter/SessionFilter.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 public class SessionFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        System.out.println("check!");
         HttpServletRequest req = (HttpServletRequest)servletRequest;
         HttpServletResponse resp = (HttpServletResponse) servletResponse;
         if(req.getSession(false) == null || req.getSession().getAttribute("member") == null){
