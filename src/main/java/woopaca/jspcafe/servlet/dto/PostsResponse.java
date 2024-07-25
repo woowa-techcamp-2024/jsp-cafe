17d5dc964f34aaf4ab242ec1c3ac8239ac5b989e
jcw1031 <jcw001031@gmail.com>
2024-07-25 14:58:55 +0900

    refactor: 게시글 목록 조회 DTO에 id 필드 추가
    - 홈 화면에서 게시글 목록을 클릭하면 id를 통해 게시글 상세 페이지를 조회할 수 있도록 추가

--- src/main/java/woopaca/jspcafe/servlet/dto/PostsResponse.java
+++ src/main/java/woopaca/jspcafe/servlet/dto/PostsResponse.java
@@ -4,10 +4,10 @@ import woopaca.jspcafe.model.Post;
 
 import java.time.format.DateTimeFormatter;
 
-public record PostsResponse(String title, String writer, String writtenAt, int viewCount) {
+public record PostsResponse(Long id, String title, String writer, String writtenAt, int viewCount) {
 
     public static PostsResponse from(Post post) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
-        return new PostsResponse(post.getTitle(), post.getWriter(), post.getWrittenAt().format(formatter), post.getViewCount());
+        return new PostsResponse(post.getId(), post.getTitle(), post.getWriter(), post.getWrittenAt().format(formatter), post.getViewCount());
     }
 }
