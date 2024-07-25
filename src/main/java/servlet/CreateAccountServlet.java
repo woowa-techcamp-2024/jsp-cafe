de69844e83360af8dca30be56dafaf1d78b9056f
hek-git <ghddmsrl1@naver.com>
2024-07-25 23:05:57 +0900

    refactor: CreateServlet 클래스 rename - 역할의 명확성을 위해 CreateAccountServlet으로 rename

--- src/main/java/servlet/CreateAccountServlet.java
+++ src/main/java/servlet/CreateAccountServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import domain.User;
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -9,20 +10,20 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import repository.UserRepository;
+import service.UserService;
+
 import java.io.IOException;
 
-@WebServlet(name = "servlet.CreateServlet", urlPatterns = {"/users"})
-public class CreateServlet extends HttpServlet {
+@WebServlet("/users")
+public class CreateAccountServlet extends HttpServlet {
 
-    public UserRepository userRepository;
-    Logger log = LoggerFactory.getLogger(CreateServlet.class);
+    private UserService userService;
+    Logger log = LoggerFactory.getLogger(CreateAccountServlet.class);
 
     @Override
-    public void init(){
-        log.info("Init CreateServlet");
-        ServletContext context = getServletContext();
-        this.userRepository = (UserRepository) context.getAttribute("repository");
+    public void init(ServletConfig config) {
+        ServletContext context = config.getServletContext();
+        this.userService = (UserService) context.getAttribute("userService");
     }
 
     @Override
@@ -32,18 +33,14 @@ public class CreateServlet extends HttpServlet {
         String password = req.getParameter("password");
         String name = req.getParameter("name");
         String email = req.getParameter("email");
-        if(userId.isEmpty() || password.isEmpty() || name.isEmpty() || email.isEmpty()) {
-            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid parameters");
-            return;
-        }
-        userRepository.saveUser(new User(userId, password, name, email));
+        userService.saveUser(new User(userId, password, name, email));
         resp.sendRedirect("/users");
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         log.info("Get /users");
-        req.setAttribute("users", userRepository.findAll());
+        req.setAttribute("users", userService.findAll());
         req.getRequestDispatcher("/user/list.jsp").forward(req, resp);
     }
 
