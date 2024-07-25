e1a7ec14716a98f44bd0cc57b8d002f31ece61d5
김수현 <i960107@naver.com>
2024-07-25 21:55:05 +0900

    fix: httpClient get 요청시 socket closed되는 문제 해결
    - try with resources 바깥에서 chunked encoding된 응답 본문 읽을 시 socket closed 에러 발생
    - try with resources 내부에서 읽어서 SavedHttpRequest 객체에 저장.

---
+++ src/test/java/com/codesquad/cafe/SavedHttpResponse.java
@@ -0,0 +1,37 @@
+package com.codesquad.cafe;
+
+import java.util.Arrays;
+import org.apache.http.Header;
+import org.apache.http.StatusLine;
+
+public class SavedHttpResponse {
+    private StatusLine statusLine;
+    private Header[] headers;
+    private String body;
+
+    public SavedHttpResponse(StatusLine statusLine, Header[] headers, String body) {
+        this.statusLine = statusLine;
+        this.headers = headers;
+        this.body = body;
+    }
+
+    public StatusLine getStatusLine() {
+        return statusLine;
+    }
+
+    public Header[] getHeaders() {
+        return headers;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public String getContentType() {
+        return Arrays.stream(headers)
+                .filter(header -> header.getName().equals("Content-Type"))
+                .findFirst()
+                .map(Header::getValue)
+                .orElse("");
+    }
+}
