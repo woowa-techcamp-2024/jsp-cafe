0e1ad379b32c75f37a17aac8fe504de656f3e30e
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-07-25 13:03:10 +0900

    [Feat] cafe 데이터베이스의 초기화 sql 파일 작성

---
+++ src/main/resources/init.sql
@@ -0,0 +1,25 @@
+-- cafe 데이터베이스가 없으면 생성
+CREATE DATABASE IF NOT EXISTS cafe;
+
+-- cafe 데이터베이스 사용
+USE cafe;
+-- 기존 테이블이 존재한다면 삭제
+DROP TABLE IF EXISTS article;
+DROP TABLE IF EXISTS member;
+
+-- member 테이블 생성
+CREATE TABLE member (
+                        id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                        memberId VARCHAR(255) UNIQUE NOT NULL,
+                        password VARCHAR(255) NOT NULL,
+                        nickname VARCHAR(255) NOT NULL,
+                        email VARCHAR(255) NOT NULL
+);
+
+-- article 테이블 생성
+CREATE TABLE article (
+                         id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                         title VARCHAR(255) NOT NULL,
+                         writer VARCHAR(255) NOT NULL,
+                         contents TEXT NOT NULL
+);
