dbcf9eb27bdf1e1b1e3dfd1cac844c640f48486c
hek-git <ghddmsrl1@naver.com>
2024-07-26 10:47:24 +0900

    feat: ArticleService 변경 - UserRepository 필드 추가 - UserRepository 생성자 주입 - UserRepository로부터 User 조회 후 게시글 작성

--- src/main/java/service/ArticleService.java
+++ src/main/java/service/ArticleService.java
@@ -1,23 +1,36 @@
 package service;
 
 import domain.Article;
-import repository.ArticleRepository;
+import domain.Users;
+import dto.ArticleDao;
+import repository.article.ArticleRepository;
+import repository.users.UserRepository;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class ArticleService {
 
     private final ArticleRepository articleRepository;
+    private final UserRepository userRepository;
 
-    public ArticleService(ArticleRepository articleRepository) {
+    public ArticleService(ArticleRepository articleRepository, UserRepository userRepository) {
         this.articleRepository = articleRepository;
+        this.userRepository = userRepository;
     }
 
     public List<Article> findAll() {
         return articleRepository.findAll();
     }
 
-    public void saveArticle(Article article) {
+    public void saveArticle(ArticleDao articleDao) {
+        Users writer = userRepository.findByUserId(articleDao.getWriter())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+        Article article = new Article(null, writer, articleDao.getTitle(), articleDao.getContent(), now.format(formatter));
         articleRepository.saveArticle(article);
     }
 
