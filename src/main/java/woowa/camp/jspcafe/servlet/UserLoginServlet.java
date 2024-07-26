734a5d887c228daa9f69a13ff3cae22290fe9869
june-777 <wlwhswnsrl96@gmail.com>
2024-07-26 10:28:34 +0900

    feat: 로그인 폼, 로그인 실패 폼 조회 기능

---
+++ src/main/java/woowa/camp/jspcafe/servlet/UserLoginServlet.java
@@ -0,0 +1,51 @@
+package woowa.camp.jspcafe.servlet;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import woowa.camp.jspcafe.domain.User;
+import woowa.camp.jspcafe.domain.exception.UserException;
+import woowa.camp.jspcafe.service.UserService;
+
+@WebServlet(name = "userLoginServlet", value = {"/users/login", "/users/login/fail"})
+public class UserLoginServlet extends HttpServlet {
+
+    private static final Logger log = LoggerFactory.getLogger(UserLoginServlet.class);
+    private UserService userService;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        log.info("UserLoginServlet init......");
+        ServletContext context = config.getServletContext();
+        userService = (UserService) context.getAttribute("userService");
+        if (this.userService == null) {
+            String errorMessage = "[ServletException] UserLoginServlet -> UserService not initialized";
+            log.error(errorMessage);
+            throw new ServletException(errorMessage);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.debug("userLoginServlet doGet start");
+        if (!req.getRequestURI().contains("fail")) {
+            RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/views/user/login.jsp");
+            requestDispatcher.forward(req, resp);
+            return;
+        }
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/views/user/login_failed.jsp");
+        requestDispatcher.forward(req, resp);
+        log.debug("userLoginServlet doGet end");
+    }
+
+}
