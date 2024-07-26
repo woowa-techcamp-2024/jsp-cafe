99af9b68f18ff0b757f2907e25fed2675dee02c6
CodingLuizy <luizy991212@gmail.com>
2024-07-26 10:36:00 +0900

    feature: 게시글에 사용자 아이디 표시

--- src/main/java/cafe/questions/servlet/QuestionServlet.java
+++ src/main/java/cafe/questions/servlet/QuestionServlet.java
@@ -3,6 +3,7 @@ package cafe.questions.servlet;
 import cafe.MappingHttpServlet;
 import cafe.questions.Article;
 import cafe.questions.repository.ArticleRepository;
+import cafe.users.repository.UserRepository;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -12,21 +13,27 @@ import java.util.List;
 
 public class QuestionServlet extends MappingHttpServlet {
     private final ArticleRepository articleRepository;
+    private final UserRepository userRepository;
 
     @Override
     public List<String> mappings() {
         return List.of("/questions/*");
     }
 
-    public QuestionServlet(ArticleRepository articleRepository) {
+    public QuestionServlet(ArticleRepository articleRepository, UserRepository userRepository) {
         this.articleRepository = articleRepository;
+        this.userRepository = userRepository;
     }
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Long id = Long.valueOf(req.getPathInfo().substring(1));
         Article article = articleRepository.findById(id);
         req.setAttribute("article", article);
+        if (article.getUserId() != null || article.getUserId() != 0) {
+            req.setAttribute("user", userRepository.findById(article.getUserId()));
+        }
         req.getRequestDispatcher("/WEB-INF/views/questions/question.jsp").forward(req, resp);
     }
 }
