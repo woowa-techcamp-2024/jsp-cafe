1661dc895832bbaa08143d9ec8b51650dd190728
donghar <donghar@naver.com>
2024-07-26 13:14:16 +0900

    [Test] MockRequestDispatcher
    Request가 dispatch되는지 확인하기 위한 mock 객체 생성

---
+++ src/test/java/codesquad/fixture/http/MockRequestDispatcher.java
@@ -0,0 +1,40 @@
+package codesquad.fixture.http;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+
+import java.io.IOException;
+
+public class MockRequestDispatcher implements RequestDispatcher {
+    private String path;
+    private boolean isForwarded = false;
+    private boolean isIncluded = false;
+
+    @Override
+    public void forward(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+        isForwarded = true;
+    }
+
+    @Override
+    public void include(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+        isIncluded = true;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public boolean isForwarded() {
+        return isForwarded;
+    }
+
+    public boolean isIncluded() {
+        return isIncluded;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
