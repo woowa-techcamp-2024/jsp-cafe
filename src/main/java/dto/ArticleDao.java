41f904664f4e06deed1f5010297a83a987319c15
hek-git <ghddmsrl1@naver.com>
2024-07-26 10:37:44 +0900

    feat: DB 접근을 위한 Dao 클래스 추가 - ArticleDao 클래스 추가 - UsersDao 클래스 추가

---
+++ src/main/java/dto/ArticleDao.java
@@ -0,0 +1,47 @@
+package dto;
+
+public class ArticleDao {
+
+    private String writer;
+    private String title;
+    private String content;
+    private String created;
+
+    public ArticleDao(String writer, String title, String content) {
+        this.writer = writer;
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+}
