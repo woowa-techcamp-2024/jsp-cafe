f7eafd739bc11e0a52de1372fd22ebcd517ca9c0
KoKimSS <seungsu8848@ajou.ac.kr>
2024-07-26 09:57:58 +0900

    feat : PathVariable추출 Util 클래스

---
+++ src/main/java/org/example/jspcafe/common/RequestUtil.java
@@ -0,0 +1,28 @@
+package org.example.jspcafe.common;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.io.IOException;
+
+import static org.example.jspcafe.common.StringUtils.isNumeric;
+
+public class RequestUtil {
+    public static Long extractPathVariable(HttpServletRequest req) throws IOException {
+        String pathInfo = getPathInfo(req);
+
+        String substring = pathInfo.substring(1);
+        if (!isNumeric(substring)) {
+            throw new IllegalArgumentException("Invalid pathInfo");
+        }
+
+        return Long.valueOf(substring);
+    }
+
+    public static String getPathInfo(HttpServletRequest req) {
+        String pathInfo = req.getPathInfo();
+        if (pathInfo == null || pathInfo.length() <= 1) {
+            throw new IllegalArgumentException("Invalid pathInfo");
+        }
+        return pathInfo;
+    }
+}
