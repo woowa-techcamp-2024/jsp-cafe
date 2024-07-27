88fe17ceb6f8ee7fb969fed146a73b74b6b0ba03
jcw1031 <jcw001031@gmail.com>
2024-07-27 10:55:56 +0900

    refactor: 객체 생성 관리용 팩토리 객체 추가
    - InstanceFactory
    - 필요한 객체는 해당 팩토리를 통해 생성하여 사용

---
+++ src/main/java/woopaca/jspcafe/config/InstanceFactory.java
@@ -0,0 +1,56 @@
+package woopaca.jspcafe.config;
+
+import woopaca.jspcafe.database.JdbcTemplate;
+import woopaca.jspcafe.repository.PostMySQLRepository;
+import woopaca.jspcafe.repository.PostRepository;
+import woopaca.jspcafe.repository.UserMySQLRepository;
+import woopaca.jspcafe.repository.UserRepository;
+import woopaca.jspcafe.service.PostService;
+import woopaca.jspcafe.service.UserService;
+
+public final class InstanceFactory {
+
+    private static UserRepository userRepository;
+    private static PostRepository postRepository;
+    private static UserService userService;
+    private static PostService postService;
+    private static JdbcTemplate jdbcTemplate;
+
+    private InstanceFactory() {
+    }
+
+    public static UserRepository userRepository() {
+        if (userRepository == null) {
+            userRepository = new UserMySQLRepository(jdbcTemplate());
+        }
+        return userRepository;
+    }
+
+    public static PostRepository postRepository() {
+        if (postRepository == null) {
+            postRepository = new PostMySQLRepository(jdbcTemplate());
+        }
+        return postRepository;
+    }
+
+    public static UserService userService() {
+        if (userService == null) {
+            userService = new UserService(userRepository());
+        }
+        return userService;
+    }
+
+    public static PostService postService() {
+        if (postService == null) {
+            postService = new PostService(postRepository(), userRepository());
+        }
+        return postService;
+    }
+
+    public static JdbcTemplate jdbcTemplate() {
+        if (jdbcTemplate == null) {
+            jdbcTemplate = new JdbcTemplate();
+        }
+        return jdbcTemplate;
+    }
+}
