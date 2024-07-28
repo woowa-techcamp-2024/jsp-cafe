eb11ed03301b401ff751418da4575ec63a5e5870
june-777 <wlwhswnsrl96@gmail.com>
2024-07-28 22:39:56 +0900

    feat: ArticleRepository 인터페이스에 update 메서드 시그니쳐 추가
    조회수 증가의 경우 update 쿼리가 필요함
    추후 게시글 수정 등 update 쿼리가 추가될 수 있으므로, DTO 객체를 파라미터로 설계함

--- src/main/java/woowa/camp/jspcafe/repository/article/ArticleRepository.java
+++ src/main/java/woowa/camp/jspcafe/repository/article/ArticleRepository.java
@@ -3,6 +3,7 @@ package woowa.camp.jspcafe.repository.article;
 import java.util.List;
 import java.util.Optional;
 import woowa.camp.jspcafe.domain.Article;
+import woowa.camp.jspcafe.repository.dto.ArticleUpdateRequest;
 
 public interface ArticleRepository {
 
@@ -15,5 +16,7 @@ public interface ArticleRepository {
     Optional<Article> findNext(Long currentId);
 
     List<Article> findByOffsetPagination(int offset, int limit);
+
+    void update(Long id, ArticleUpdateRequest articleUpdateRequest);
 
 }
