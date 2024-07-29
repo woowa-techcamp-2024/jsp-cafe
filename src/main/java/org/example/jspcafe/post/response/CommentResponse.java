9eb6495f25312e79994a686f35020104dd48b13b
miiiinju1 <gms07073@ynu.ac.kr>
2024-07-29 23:11:36 +0900

    feat: CommentResponse에 commentId, postId를 String에서 Long타입으로 변경

--- src/main/java/org/example/jspcafe/post/response/CommentResponse.java
+++ src/main/java/org/example/jspcafe/post/response/CommentResponse.java
@@ -3,9 +3,9 @@ package org.example.jspcafe.post.response;
 import java.time.LocalDateTime;
 
 public record CommentResponse(
-        String commentId,
+        Long commentId,
         Long userId,
-        String postId,
+        Long postId,
         String nickname,
         String content,
         LocalDateTime createdAt
