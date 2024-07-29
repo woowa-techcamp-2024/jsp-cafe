52c705acdf95aff1423fe36b8ad46656aba206a9
huiseung <whtjddn75@gmail.com>
2024-07-29 16:46:24 +0900

    [feature] session 확인 filter 추가

---
+++ src/main/java/org/example/filter/AuthenticationFilter.java
@@ -0,0 +1,44 @@
+package org.example.filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+import org.example.constance.SessionName;
+
+@WebFilter(
+        urlPatterns = {"/articles/register", "/articles/*"}
+)
+public class AuthenticationFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+        if ("GET".equalsIgnoreCase(httpServletRequest.getMethod())) {
+            HttpSession session = httpServletRequest.getSession(false);
+            if (session == null || session.getAttribute(SessionName.USER.getName()) == null) {
+                httpServletResponse.sendRedirect("/login");
+                return;
+            }
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
