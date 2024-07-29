ef10726ff73d1de7881858b717ea5d506d1ad8c9
miiiinju1 <gms07073@ynu.ac.kr>
2024-07-29 20:17:38 +0900

    feat: Comment 기록에 userId 필드 추가

--- src/main/java/org/example/jspcafe/post/response/CommentResponse.java
+++ src/main/java/org/example/jspcafe/post/response/CommentResponse.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 public record CommentResponse(
         String commentId,
+        Long userId,
         String postId,
         String nickname,
         String content,
