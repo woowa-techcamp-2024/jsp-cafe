4f0e06854f34f2d69584fe471e4f26ae079a9521
김수현 <i960107@naver.com>
2024-07-29 10:04:25 +0900

    feat: PostRepository 인터페이스 추가
    - InmemoryPostRepository가 오버라이드 할 필요 없도록 디폴드 메서드 추가

--- src/main/java/com/codesquad/cafe/db/PostRepository.java
+++ src/main/java/com/codesquad/cafe/db/PostRepository.java
@@ -1,7 +1,8 @@
 package com.codesquad.cafe.db;
 
-import com.codesquad.cafe.model.Post;
-import com.codesquad.cafe.model.PostDetailsDto;
+import com.codesquad.cafe.exception.UnsupportedDBOperationException;
+import com.codesquad.cafe.db.entity.Post;
+import com.codesquad.cafe.db.entity.PostDetailsDto;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,11 +12,26 @@ public interface PostRepository {
 
     Optional<Post> findById(Long id);
 
+    default boolean existsById(Long id){
+        throw new UnsupportedDBOperationException("existsById is not supported");
+    }
+
+    int countAll();
+
     List<Post> findAll();
 
-    Page<Post> findByPage(int pageNum, int pageSize);
+    Page<PostDetailsDto> findPostWithAuthorByPageSortByCreatedAtDesc(int pageNum, int pageSize);
+
+    Optional<PostDetailsDto> findPostWithAuthorById(Long id);
 
     void deleteAll();
 
-    List<PostDetailsDto> findAllPostWithDetail();
+    default void deleteById(Long Id) {
+        throw new UnsupportedDBOperationException("deleteById is not supported");
+    }
+
+    default void addView(Post post) {
+        throw new UnsupportedDBOperationException("addView is not supported");
+    }
+
 }
