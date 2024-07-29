6c0f9b8157165f2a685c7e93fa5b98c308e8d5a1
KyungMin Lee <hello.tidavid@gmail.com>
2024-07-30 01:42:21 +0900

    feat: 질문 갱신 페이지 이동 기능 추가
    - `/questions/{articleId}/form` GET URI를 통해 질문 수정 페이지로 이동 가능하도록 했습니다.

--- src/main/java/codesquad/jspcafe/domain/article/controller/ArticleServlet.java
+++ src/main/java/codesquad/jspcafe/domain/article/controller/ArticleServlet.java
@@ -8,11 +8,12 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.file.AccessDeniedException;
 import java.util.NoSuchElementException;
 
 
 /**
- * ArticleServlet은 질문 조회 요청을 처리하는 서블릿입니다. <br> GET 메서드를 제공하여 질문을 표시합니다.
+ * ArticleServlet은 질문 조회 요청을 처리하는 서블릿입니다. <br> GET 메서드를 제공하여 질문을 표시하거나 질문 갱신 페이지로 이동합니다.
  */
 @WebServlet("/questions/*")
 public class ArticleServlet extends HttpServlet {
@@ -31,9 +32,12 @@ public class ArticleServlet extends HttpServlet {
 
 
     /**
-     * GET 요청을 처리하여 question.jsp 페이지로 포워딩합니다. <br> 클라이언트가 /questions/{articleId}로 GET 요청을 보낼 때 이
-     * 메서드가 호출됩니다. <br> articleId가 존재하지 않는 경우 NoSuchElementException{@link NoSuchElementException}
-     * 예외를 던집니다.
+     * GET 요청을 처리하여 여러 .jsp 페이지로 포워딩합니다.<br> 클라이언트가 /questions/{articleId}로 GET 요청을 보낼
+     * 때,/questions/{articleId}/form으로 GET 요청을 보낼 때 이 메서드가 호출됩니다. <br><br> /questions/{articleId}:
+     * 질문을 페이지(question.jsp)로 포워딩합니다. <br> /questions/{articleId}/form: 질문 수정
+     * 페이지(questionUpdateForm.jsp)로 포워딩합니다. <br> 수정 요청에 대해 작성자가 아닌경우
+     * AccessDeniedException{@link AccessDeniedException}을 던집니다. <br> 요청 URI가 잘못된 경우
+     * NoSuchElementException{@link NoSuchElementException}을 던집니다.
      *
      * @param req  an {@link HttpServletRequest} 클라이언트가 서블릿에 보낸 요청을 포함하는 HttpServletRequest 객체
      * @param resp an {@link HttpServletResponse} 서블릿이 클라이언트에게 보내는 응답을 포함하는 HttpServletResponse 객체
@@ -45,6 +49,18 @@ public class ArticleServlet extends HttpServlet {
         throws ServletException, IOException {
         String pathInfo = req.getPathInfo();
         verifyPathInfo(pathInfo);
+        if (pathInfo.endsWith("/form")) {
+            String articleId = pathInfo.substring(1, pathInfo.length() - "/form".length());
+            ArticleCommonResponse articleCommonResponse = articleService.getArticleById(
+                articleId);
+            if (articleCommonResponse.getWriterUserId() != req.getSession()
+                .getAttribute("userId")) {
+                throw new AccessDeniedException("접근 권한이 없습니다.");
+            }
+            req.setAttribute("article", articleCommonResponse);
+            req.getRequestDispatcher("/WEB-INF/jsp/questionUpdateForm.jsp").forward(req, resp);
+            return;
+        }
         String articleId = pathInfo.substring(1);
         ArticleCommonResponse articleCommonResponse = articleService.getArticleById(
             articleId);
---
+++ src/main/webapp/WEB-INF/jsp/questionUpdateForm.jsp
@@ -0,0 +1,63 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: davidlee
+  Date: 24. 7. 30.
+  Time: 오전 1:08
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE>
+<html lang="ko">
+<jsp:include page="/WEB-INF/jsp/component/headers.jsp"/>
+<body>
+<jsp:include page="/WEB-INF/jsp/component/navbar.jsp"/>
+
+<div class="container" id="main">
+    <div class="col-md-12 col-sm-12 col-lg-10 col-lg-offset-1">
+        <div class="panel panel-default content-main">
+            <c:set var="articleCommonResponse" value="${requestScope.article}"/>
+            <form id="question" name="question" method="post"
+                  action="/questions/${articleCommonResponse.id}">
+                <input type="hidden" name="id" value="${articleCommonResponse.id}"/>
+                <div class="form-group">
+                    <label for="title">제목</label>
+                    <input type="text" class="form-control" id="title" name="title" placeholder="제목"
+                           value="${articleCommonResponse.title}"/>
+                </div>
+                <div class="form-group">
+                    <label for="contents">내용</label>
+                    <textarea name="contents" id="contents" rows="5"
+                              class="form-control">${articleCommonResponse.contents}</textarea>
+                </div>
+                <button type="submit" class="btn btn-success clearfix pull-right">수정하기</button>
+                <div class="clearfix"></div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<jsp:include page="/WEB-INF/jsp/component/scripts.jsp"/>
+</body>
+</html>
+
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+        crossorigin="anonymous"></script>
+<script>
+  $(document).ready(function () {
+    $('#question').on('submit', function (e) {
+      e.preventDefault(); // Prevent the form from submitting the default way
+
+      let form = $(this);
+      let actionUrl = form.attr('action');
+      let formData = form.serialize(); // Serialize form data
+
+      $.ajax({
+        url: actionUrl,
+        type: 'PUT', // Use PUT method
+        data: formData,
+      });
+    })
+  });
+</script>
