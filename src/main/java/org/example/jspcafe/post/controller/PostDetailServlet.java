c771f35174f383494ae7fcf7c16bfe39a940542c
miiiinju1 <gms07073@ynu.ac.kr>
2024-07-29 23:12:45 +0900

    feat: PostDetail을 가져올 때, 빈 Comment List에서 실제 Comment List를 반환하도록 변경

--- src/main/java/org/example/jspcafe/post/controller/PostDetailServlet.java
+++ src/main/java/org/example/jspcafe/post/controller/PostDetailServlet.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import org.example.jspcafe.comment.service.CommentService;
 import org.example.jspcafe.di.ApplicationContext;
 import org.example.jspcafe.post.response.CommentResponse;
 import org.example.jspcafe.post.response.PostResponse;
@@ -18,11 +19,13 @@ import java.util.List;
 public class PostDetailServlet extends HttpServlet {
 
     private PostService postService;
+    private CommentService commentService;
 
     @Override
     public void init() throws ServletException {
         super.init();
         this.postService = ApplicationContext.getContainer().resolve(PostService.class);
+        this.commentService = ApplicationContext.getContainer().resolve(CommentService.class);
     }
 
     @Override
@@ -39,9 +42,12 @@ public class PostDetailServlet extends HttpServlet {
             return;
         }
 
-        String postId = pathInfo.substring(1);
-        PostResponse post = postService.getPost(postId);
-        List<CommentResponse> comments = List.of();
+        String postIdStr = pathInfo.substring(1);
+        PostResponse post = postService.getPost(postIdStr);
+
+        Long postId = Long.parseLong(postIdStr);
+
+        List<CommentResponse> comments = commentService.findCommentsJoinUser(postId);
 
         String commentWriter = (String) req.getSession().getAttribute("nickname");
         if(commentWriter== null) {
