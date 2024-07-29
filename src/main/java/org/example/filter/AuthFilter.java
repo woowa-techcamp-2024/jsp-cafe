fd88228be75b20b5e23accf9dc010cd239ab23c9
mirageoasis <kimhw0820@naver.com>
2024-07-29 17:38:43 +0900

    feat: auth filter 수정

--- src/main/java/org/example/filter/AuthFilter.java
+++ src/main/java/org/example/filter/AuthFilter.java
@@ -11,8 +11,15 @@ import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.regex.Pattern;
+import org.example.entity.User;
+import org.example.repository.UserRepository;
+import org.example.repository.UserRepositoryDBImpl;
+import org.example.service.UserService;
 import org.example.util.LoggerUtil;
+import org.example.util.SessionUtil;
 import org.slf4j.Logger;
 
 @WebFilter(
@@ -28,8 +35,10 @@ public class AuthFilter implements Filter {
 
     List<String> exactPath = List.of(
         "/qna/form.html",
-        "static/qna/form.html"
+        "/static/qna/form.html"
     );
+
+    private final UserRepository userRepository = UserRepositoryDBImpl.getInstance();
 
     @Override
     public void destroy() {	}
@@ -40,9 +49,13 @@ public class AuthFilter implements Filter {
 
         HttpServletRequest httpRequest = (HttpServletRequest) request;
 
+        Optional<String> userId = SessionUtil.extractUserId(httpRequest);
+
+        Optional<User> user = userId.map(userRepository::getUserByUserId).orElse(null);
+
         for(Pattern pattern : checkPatterns){
             if(pattern.matcher(httpRequest.getRequestURI()).matches()){
-                if(httpRequest.getSession().getAttribute("userId") == null){
+                if(userId.isEmpty() || user.isEmpty()){
                     httpRequest.getRequestDispatcher("/login").forward(request, response);
                     return;
                 }
@@ -50,7 +63,7 @@ public class AuthFilter implements Filter {
         }
         for(String path : exactPath){
             if(httpRequest.getRequestURI().equals(path)){
-                if(httpRequest.getSession().getAttribute("userId") == null){
+                if(userId.isEmpty() || user.isEmpty()){
                     httpRequest.getRequestDispatcher("/login").forward(request, response);
                     return;
                 }
