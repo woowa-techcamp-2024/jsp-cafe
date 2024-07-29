bd9e11b2c6cbd989bea342577fafe9b5ca2ec6b1
jcw1031 <jcw001031@gmail.com>
2024-07-29 23:45:29 +0900

    refactor: 레코드 클래스로 변경
    - Authentication 클래스를 레코드 클래스로 변경

--- src/main/java/woopaca/jspcafe/model/Authentication.java
+++ src/main/java/woopaca/jspcafe/model/Authentication.java
@@ -2,13 +2,5 @@ package woopaca.jspcafe.model;
 
 import java.time.LocalDateTime;
 
-public class Authentication {
-
-    private final User principal;
-    private final LocalDateTime authenticatedAt;
-
-    public Authentication(User principal, LocalDateTime authenticatedAt) {
-        this.principal = principal;
-        this.authenticatedAt = authenticatedAt;
-    }
+public record Authentication(User principal, LocalDateTime authenticatedAt) {
 }
