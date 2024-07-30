8a2c243c29337ab69b85f50e3a65d566f5620553
kimhyun5u <22kimhyun5u@gmail.com>
2024-07-30 18:03:49 +0900

    feat: ReplyService.deleteById 구현

--- src/main/java/camp/woowa/jspcafe/service/ReplyService.java
+++ src/main/java/camp/woowa/jspcafe/service/ReplyService.java
@@ -1,5 +1,7 @@
 package camp.woowa.jspcafe.service;
 
+import camp.woowa.jspcafe.exception.CustomException;
+import camp.woowa.jspcafe.exception.HttpStatus;
 import camp.woowa.jspcafe.model.Reply;
 import camp.woowa.jspcafe.repository.ReplyRepository;
 
@@ -22,5 +24,13 @@ public class ReplyService {
 
     public List<Reply> findByQuestionId(Long questionId) {
         return replyRepository.findByQuestionId(questionId);
+    }
+
+    public void deleteById(Long id, long writerId) {
+        Reply reply = replyRepository.findById(id);
+        if (reply.getWriterId() != writerId) { // 작성자인지 확인
+            throw new CustomException(HttpStatus.FORBIDDEN, "작성자만 삭제할 수 있습니다.");
+        }
+        replyRepository.deleteById(id);
     }
 }
