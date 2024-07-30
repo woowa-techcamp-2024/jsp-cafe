f5ee15190ed5b79618302b1fc60452d5e3ccea1d
june-777 <wlwhswnsrl96@gmail.com>
2024-07-30 17:19:24 +0900

    feat: 수정할 게시글 조회 응답 객체

---
+++ src/main/java/woowa/camp/jspcafe/service/dto/ArticleUpdateResponse.java
@@ -0,0 +1,32 @@
+package woowa.camp.jspcafe.service.dto;
+
+import woowa.camp.jspcafe.domain.Article;
+
+public class ArticleUpdateResponse {
+
+    private Long id;
+    private String title;
+    private String content;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    private ArticleUpdateResponse(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public static ArticleUpdateResponse from(Article article) {
+        return new ArticleUpdateResponse(article.getId(), article.getTitle(), article.getContent());
+    }
+}
