eaf776768c6867aaa4353e814811f7e619fdda1d
seminchoi <choicco89@gmail.com>
2024-07-30 14:20:40 +0900

    test: ArticleTest Article 모델 필드 변경사항에 맞게 테스트 변경

--- src/test/java/codesqaud/app/model/ArticleTest.java
+++ src/test/java/codesqaud/app/model/ArticleTest.java
@@ -2,6 +2,7 @@ package codesqaud.app.model;
 
 import codesqaud.app.exception.HttpException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
@@ -12,10 +13,11 @@ public class ArticleTest {
 
     private String title = "title";
     private String contents = "Sample contents of the article.";
-    private String authorId = "author123";
+    private Long authorId = 1L;
 
     @Nested
-    class 생성자_유효성_검사 {
+    @DisplayName("생성자 유효성 검사 시")
+    class ConstructorTest {
         @Test
         void title이_null이면_예외가_발생한다() {
             assertThatThrownBy(() -> new Article(null, contents, authorId))
@@ -47,15 +49,7 @@ public class ArticleTest {
         @Test
         void authorId가_null이면_예외가_발생한다() {
             assertThatThrownBy(() -> new Article(title, contents, null))
-                    .isInstanceOf(HttpException.class)
-                    .hasMessageContaining("작성자 ID는 null이거나 비어있을 수 없습니다.");
-        }
-
-        @Test
-        void authorId가_빈_문자열이면_예외가_발생한다() {
-            assertThatThrownBy(() -> new Article(title, contents, ""))
-                    .isInstanceOf(HttpException.class)
-                    .hasMessageContaining("작성자 ID는 null이거나 비어있을 수 없습니다.");
+                    .isInstanceOf(HttpException.class);
         }
 
         @Test
@@ -69,7 +63,8 @@ public class ArticleTest {
     }
 
     @Nested
-    class Setter_유효성_검사 {
+    @DisplayName("Setter 유효성 검사 시")
+    class SetterTest {
         private Article article;
 
         @BeforeEach
@@ -106,28 +101,12 @@ public class ArticleTest {
         }
 
         @Test
-        void authorId가_null이면_예외가_발생한다() {
-            assertThatThrownBy(() -> article.setAuthorId(null))
-                    .isInstanceOf(HttpException.class)
-                    .hasMessageContaining("작성자 ID는 null이거나 비어있을 수 없습니다.");
-        }
-
-        @Test
-        void authorId가_빈_문자열이면_예외가_발생한다() {
-            assertThatThrownBy(() -> article.setAuthorId(""))
-                    .isInstanceOf(HttpException.class)
-                    .hasMessageContaining("작성자 ID는 null이거나 비어있을 수 없습니다.");
-        }
-
-        @Test
         void 올바른_값으로_설정하면_정상적으로_반영된다() {
             article.setTitle("newTitle");
             article.setContents("newContents");
-            article.setAuthorId("newAuthorId");
 
             assertThat(article.getTitle()).isEqualTo("newTitle");
             assertThat(article.getContents()).isEqualTo("newContents");
-            assertThat(article.getAuthorId()).isEqualTo("newAuthorId");
         }
     }
 }
