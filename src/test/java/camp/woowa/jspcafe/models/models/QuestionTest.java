42dfaa101749a184f0486c2674e8ca1db2057887
kimhyun5u <22kimhyun5u@gmail.com>
2024-07-30 10:36:02 +0900

    test: Question.update 테스트 추가

--- src/test/java/camp/woowa/jspcafe/models/models/QuestionTest.java
+++ src/test/java/camp/woowa/jspcafe/models/models/QuestionTest.java
@@ -23,4 +23,22 @@ class QuestionTest {
         assertEquals(content, question.getContent());
         assertEquals(writer, question.getWriter());
     }
+
+    @Test
+    void testUpdateQuestion() {
+        // given
+        Long id = 1L;
+        String title = "title";
+        String content = "content";
+        String writer = "1234";
+        Question question = new Question(id, title, content, writer, 1L);
+        String updatedTitle = "updatedTitle";
+        String updatedContent = "updatedContent";
+        // when
+        question.update(updatedTitle, updatedContent);
+
+        // then
+        assertEquals(question.getTitle(), updatedTitle);
+        assertEquals(question.getContent(), updatedContent);
+    }
 }
