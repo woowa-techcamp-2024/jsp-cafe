a58530eef82ab17e7d680e5115b5cec169efeea5
jcw1031 <jcw001031@gmail.com>
2024-07-30 15:13:27 +0900

    feat: 게시글 수정 폼 서블릿 추가
    - 게시글 수정 요청에 대한 폼 반환
    - 만약 다른 사용자의 게시글을 수정하려고 하는 경우 403 Forbidden 응답

---
+++ src/main/java/woopaca/jspcafe/servlet/PostEditServlet.java
@@ -0,0 +1,48 @@
+package woopaca.jspcafe.servlet;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import woopaca.jspcafe.model.Authentication;
+import woopaca.jspcafe.service.PostService;
+import woopaca.jspcafe.servlet.dto.response.PostEditResponse;
+
+import java.io.IOException;
+
+@WebServlet("/posts/edit/*")
+public class PostEditServlet extends HttpServlet {
+
+    private PostService postService;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        ServletContext servletContext = config.getServletContext();
+        this.postService = (PostService) servletContext.getAttribute("postService");
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String pathInfo = request.getPathInfo();
+        Long postId = Long.parseLong(pathInfo.substring(1));
+        HttpSession session = request.getSession();
+        Authentication authentication = (Authentication) session.getAttribute("authentication");
+        postService.validateWriter(postId, authentication);
+        super.service(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String pathInfo = request.getPathInfo();
+        Long postId = Long.parseLong(pathInfo.substring(1));
+        PostEditResponse postEditResponse = postService.getPostTitleContent(postId);
+        request.setAttribute("post", postEditResponse);
+        request.getRequestDispatcher("/post/edit.jsp")
+                .forward(request, response);
+    }
+}
---
+++ src/main/webapp/post/edit.jsp
@@ -0,0 +1,34 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="${pageContext.request.contextPath}/static/css/style.css">
+  <link rel="stylesheet" href="${pageContext.request.contextPath}/static/css/main.css">
+  <link rel="stylesheet" href="${pageContext.request.contextPath}/static/css/post-form.css">
+  <title>찬우 카페</title>
+</head>
+<body>
+<div id="container">
+  <jsp:include page="${pageContext.request.contextPath}/common/header.jsp" />
+  <form id="post-form" method="post" action="${pageContext.request.contextPath}/posts/edit/${post.id()}">
+    <div id="form-title">
+      글 수정
+    </div>
+    <div class="form-element">
+      <div class="form-label">제목</div>
+      <input type="text" autofocus required minlength="2" maxlength="30" id="title" name="title"
+             placeholder="글의 제목을 입력해주세요" value="<c:out value="${post.title()}" />">
+    </div>
+    <div class="form-element">
+      <div class="form-label">내용</div>
+      <textarea name="content" required minlength="2" maxlength="1000" id="content"
+                placeholder="글의 내용을 입력해주세요"><c:out value="${post.content()}" /></textarea>
+    </div>
+    <button id="post-submit">수정 완료!</button>
+  </form>
+</div>
+</body>
+</html>
