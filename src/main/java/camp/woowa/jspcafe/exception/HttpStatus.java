b472a6f08e203d2ca5944c2a4b8291a9e0f39617
kimhyun5u <22kimhyun5u@gmail.com>
2024-07-30 11:14:53 +0900

    add: METHOD_NOT_ALLOWED 추

--- src/main/java/camp/woowa/jspcafe/exception/HttpStatus.java
+++ src/main/java/camp/woowa/jspcafe/exception/HttpStatus.java
@@ -9,6 +9,7 @@ public enum HttpStatus {
     UNAUTHORIZED(401, "Unauthorized", ""),
     FORBIDDEN(403, "Forbidden", ""),
     NOT_FOUND(404, "Not Found", ""),
+    METHOD_NOT_ALLOWED(405, "Method Not Allowed", ""),
     INTERNAL_SERVER_ERROR(500, "Internal Server Error", ""),
 
     // custom status
