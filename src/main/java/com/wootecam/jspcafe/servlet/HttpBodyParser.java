ba25ecd9339b139023a852c7366d459339780341
HiiWee <wpdlzhf159@gmail.com>
2024-07-30 15:25:15 +0900

    feat: Http Message Body Parser 추가
    - form data를 파싱합니다.
    - value나 키가 없다면 파싱하지 않음

---
+++ src/main/java/com/wootecam/jspcafe/servlet/HttpBodyParser.java
@@ -0,0 +1,24 @@
+package com.wootecam.jspcafe.servlet;
+
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class HttpBodyParser {
+
+    private static final int KEY_VALUE_LENGTH = 2;
+
+    private HttpBodyParser() {
+    }
+
+    public static Map<String, String> parse(final String bodyData) {
+        String decodedData = URLDecoder.decode(bodyData, StandardCharsets.UTF_8);
+
+        return Arrays.stream(decodedData.split("&"))
+                .map(param -> param.split("="))
+                .filter(params -> params.length == KEY_VALUE_LENGTH)
+                .collect(Collectors.toMap(splitParams -> splitParams[0].trim(), splitParams -> splitParams[1].trim()));
+    }
+}
---
+++ src/test/java/com/wootecam/jspcafe/servlet/HttpBodyParserTest.java
@@ -0,0 +1,35 @@
+package com.wootecam.jspcafe.servlet;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+class HttpBodyParserTest {
+
+    @Test
+    void 바디데이터에서_파라미터를_파싱할_수_있다() {
+        // given
+        String bodyData = "title=%EB%B0%98%EA%B0%91%EC%8A%B5%EB%8B%88%EB%8B%A4!&contents=%EC%95%88%EB%85%95%ED%95%98%EC%84%B8%EC%9A%94!";
+
+        // when
+        Map<String, String> parse = HttpBodyParser.parse(bodyData);
+
+        // then
+        assertThat(parse)
+                .containsExactlyInAnyOrderEntriesOf(Map.of("title", "반갑습니다!", "contents", "안녕하세요!"));
+    }
+
+    @Test
+    void value가_없다면_null로_파싱한다() {
+        // given
+        String bodyData = "title=%EB%B0%98%EA%B0%91%EC%8A%B5%EB%8B%88%EB%8B%A4!&contents=";
+
+        // when
+        String contents = HttpBodyParser.parse(bodyData)
+                .get("contents");
+
+        // then
+        assertThat(contents).isNull();
+    }
+}
