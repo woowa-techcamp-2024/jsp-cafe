6053bbbe1d0b7bb80c2fad59bd8d99f3c264aaca
김수현 <i960107@naver.com>
2024-07-30 13:18:34 +0900

    feat: 로그아웃 기능 구현
    - 세션 만료

---
+++ src/main/java/com/codesquad/cafe/servlet/LogoutServlet.java
@@ -0,0 +1,23 @@
+package com.codesquad.cafe.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LogoutServlet extends UserServlet {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        resp.sendRedirect("/");
+    }
+}
