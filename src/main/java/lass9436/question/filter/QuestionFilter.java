bf7a40692013c9d6bc9209f687e40fe4ba8d6321
lass9436 <lass9436@gmail.com>
2024-07-30 13:46:05 +0900

    [feat] QuestionFilter 작성
    글 필터를 작성하여 로그인한 사람만 글의 등록 및 수정 삭제를 할 수 있도록 하였습니다.

---
+++ src/main/java/lass9436/question/filter/QuestionFilter.java
@@ -0,0 +1,47 @@
+package lass9436.question.filter;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebFilter({"/questions/*", "/questions"})
+public class QuestionFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws
+		IOException,
+		ServletException {
+		if (servletRequest instanceof HttpServletRequest req && servletResponse instanceof HttpServletResponse resp) {
+			HttpSession session = req.getSession(false);
+			if (session != null) {
+				String userId = (String) session.getAttribute("userId");
+				if (userId != null) {
+					filterChain.doFilter(req, resp);
+					return;
+				}
+				resp.sendRedirect("/users/login");
+				return;
+			}
+			resp.sendRedirect("/users/login");
+		}
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		Filter.super.init(filterConfig);
+	}
+
+	@Override
+	public void destroy() {
+		Filter.super.destroy();
+	}
+}
