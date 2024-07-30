2cf261afff7f864e92bed401c01d493c5c92565b
kimhyun5u <22kimhyun5u@gmail.com>
2024-07-30 15:35:47 +0900

    add: ReplyRepository 테스트 추가 - ReplyRepository.findByQuestionId

--- src/test/java/camp/woowa/jspcafe/repository/ReplyRepositoryTest.java
+++ src/test/java/camp/woowa/jspcafe/repository/ReplyRepositoryTest.java
@@ -4,6 +4,8 @@ import camp.woowa.jspcafe.model.Reply;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ReplyRepositoryTest {
@@ -32,5 +34,24 @@ class ReplyRepositoryTest {
         assertEquals(reply.getQuestionId(), questionId);
         assertEquals(reply.getWriter(), writer);
         assertEquals(reply.getWriterId(), writerId);
+    }
+
+    @Test
+    void testFindByQuestionId() {
+        // given
+        Long questionId = 1L;
+        Long writerId = 1L;
+        String writer = "writer";
+        String content = "content";
+        int iteration = 3;
+        for (int i = 0; i < iteration; i++)
+            replyRepository.save(new Reply(content, questionId, writer, writerId));
+
+
+        // when
+        List<Reply> reply = replyRepository.findByQuestionId(questionId);
+
+        // then
+        assertEquals(iteration, reply.size());
     }
 }
