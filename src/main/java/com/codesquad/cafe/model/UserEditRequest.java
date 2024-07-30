24229fad21554790754c8df4bae9449dde4075b2
김수현 <i960107@naver.com>
2024-07-30 17:54:49 +0900

    feat: request dto 유효성 검사 추가
    - Dto에 유효성 검증 책임
    - 유효성 검증 실패시 ValidationException 던짐
    - 빈 string 비교 위해 StringUilts 편의 메서드 추가

--- src/main/java/com/codesquad/cafe/model/UserEditRequest.java
+++ src/main/java/com/codesquad/cafe/model/UserEditRequest.java
@@ -1,20 +1,38 @@
 package com.codesquad.cafe.model;
 
+import com.codesquad.cafe.exception.ValidationException;
+import com.codesquad.cafe.util.StringUtils;
+
 public class UserEditRequest {
 
     private Long id;
 
     private String username;
 
+    private String originalPassword;
+
     private String password;
 
-    private String originalPassword;
 
     private String confirmPassword;
 
     private String name;
 
     private String email;
+
+    public UserEditRequest() {
+    }
+
+    public UserEditRequest(Long id, String username, String password, String originalPassword, String confirmPassword,
+                           String name, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.originalPassword = originalPassword;
+        this.confirmPassword = confirmPassword;
+        this.name = name;
+        this.email = email;
+    }
 
     public Long getId() {
         return id;
@@ -44,33 +62,46 @@ public class UserEditRequest {
         return email;
     }
 
-    public boolean isValid() {
+    public void validate() {
+        StringBuilder sb = new StringBuilder();
+        boolean valid = true;
         if (id == null || id <= 0) {
-            return false;
+            sb.append("id는 1이상의 자연수여야 합니다.").append("\n");
+            valid = false;
         }
-        if (username == null || username.isBlank()) {
-            return false;
+        if (StringUtils.isBlank(username)) {
+            sb.append("username은 필수입니다.").append("\n");
+            valid = false;
         }
-        if (password == null || password.isBlank()) {
-            return false;
+        if (StringUtils.isBlank(originalPassword)) {
+            sb.append("originalPassword는 필수입니다.").append("\n");
+            valid = false;
         }
-        if (originalPassword == null || originalPassword.isBlank()) {
-            return false;
+        if (StringUtils.isBlank(password)) {
+            sb.append("password는 필수입니다.").append("\n");
+            valid = false;
         }
-        if (confirmPassword == null || confirmPassword.isBlank()) {
-            return false;
+        if (StringUtils.isBlank(confirmPassword)) {
+            sb.append("confirmPassword는 필수입니다.").append("\n");
+            valid = false;
         }
-        if (name == null || name.isBlank()) {
-            return false;
+        if (StringUtils.isBlank(name)) {
+            sb.append("name은 필수입니다.").append("\n");
+            valid = false;
         }
-        if (email == null || email.isBlank()) {
-            return false;
+        if (StringUtils.isBlank(email)) {
+            sb.append("email은 필수입니다.").append("\n");
+            valid = false;
+        } else {
+            String[] emailTokens = email.split("@");
+            if (emailTokens.length != 2 || emailTokens[0].isBlank() || emailTokens[1].isBlank()) {
+                sb.append("잘못된 email 형식입니다.").append("\n");
+                valid = false;
+            }
         }
-        String[] emailTokens = email.split("@");
-        if (emailTokens.length != 2 || emailTokens[0].isBlank() || emailTokens[1].isBlank()) {
-            return false;
+        if (!valid) {
+            throw new ValidationException(sb.toString());
         }
-        return true;
     }
 
 }
--- src/main/java/com/codesquad/cafe/model/UserJoinRequest.java
+++ src/main/java/com/codesquad/cafe/model/UserJoinRequest.java
@@ -1,6 +1,8 @@
 package com.codesquad.cafe.model;
 
 import com.codesquad.cafe.db.entity.User;
+import com.codesquad.cafe.exception.ValidationException;
+import com.codesquad.cafe.util.StringUtils;
 
 public class UserJoinRequest {
 
@@ -28,8 +30,48 @@ public class UserJoinRequest {
         return email;
     }
 
+    public UserJoinRequest() {
+    }
+
+    public UserJoinRequest(String username, String password, String name, String email) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
     public User toUser() {
         return User.of(username, password, name, email);
+    }
+
+    public void validate() {
+        StringBuilder sb = new StringBuilder();
+        boolean valid = true;
+        if (StringUtils.isBlank(username)) {
+            sb.append("username은 필수입니다.").append("\n");
+            valid = false;
+        }
+        if (StringUtils.isBlank(password)) {
+            sb.append("password는 필수입니다.").append("\n");
+            valid = false;
+        }
+        if (StringUtils.isBlank(name)) {
+            sb.append("name은 필수입니다.").append("\n");
+            valid = false;
+        }
+        if (StringUtils.isBlank(email)) {
+            sb.append("email은 필수입니다.").append("\n");
+            valid = false;
+        } else {
+            String[] emailTokens = email.split("@");
+            if (emailTokens.length != 2 || emailTokens[0].isBlank() || emailTokens[1].isBlank()) {
+                sb.append("잘못된 email 형식입니다.").append("\n");
+                valid = false;
+            }
+        }
+        if (!valid) {
+            throw new ValidationException(sb.toString());
+        }
     }
 
     @Override
