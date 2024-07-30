d65bc6aa8f3a8cbf7486a01cc70064d87e2208ac
huiseung <whtjddn75@gmail.com>
2024-07-30 15:32:14 +0900

    [feature] 댓글 클래스, data handler

---
+++ src/main/java/org/example/domain/Reply.java
@@ -0,0 +1,68 @@
+package org.example.domain;
+
+import java.time.LocalDateTime;
+import org.example.constance.AliveStatus;
+
+public class Reply {
+    private Long replyId;
+    private Long userId;
+    private Long articleId;
+    private String author;
+    private String comment;
+    private AliveStatus aliveStatus;
+    private LocalDateTime createdDt;
+
+
+    public Reply(Long replyId, Long userId, Long articleId, String author, String comment, AliveStatus aliveStatus,
+                 LocalDateTime createdDt) {
+        this.replyId = replyId;
+        this.userId = userId;
+        this.articleId = articleId;
+        this.author = author;
+        this.comment = comment;
+        this.aliveStatus = aliveStatus;
+        this.createdDt = createdDt;
+    }
+
+    public Reply(Long userId, Long articleId, String author, String comment, AliveStatus aliveStatus,
+                 LocalDateTime createdDt) {
+        this.userId = userId;
+        this.articleId = articleId;
+        this.author = author;
+        this.comment = comment;
+        this.aliveStatus = aliveStatus;
+        this.createdDt = createdDt;
+    }
+
+    public Long getReplyId() {
+        return replyId;
+    }
+
+    public Long getArticleId() {
+        return articleId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public AliveStatus getAliveStatus() {
+        return aliveStatus;
+    }
+
+    public LocalDateTime getCreatedDt() {
+        return createdDt;
+    }
+
+    public void delete() {
+        this.aliveStatus = AliveStatus.DELETED;
+    }
+}
