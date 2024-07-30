98e7a7a51016f9467d67c09121d9454f320127ee
김수현 <i960107@naver.com>
2024-07-30 13:40:39 +0900

    feat: sessionId 추가한  get, post 요청 추가
    - 헤더에 Cookie 헤더 추가

--- src/test/java/com/codesquad/cafe/E2ETestBase.java
+++ src/test/java/com/codesquad/cafe/E2ETestBase.java
@@ -11,19 +11,24 @@ import org.apache.catalina.startup.Tomcat;
 import org.apache.catalina.valves.ErrorReportValve;
 import org.apache.catalina.webresources.DirResourceSet;
 import org.apache.catalina.webresources.StandardRoot;
+import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.RedirectStrategy;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.EntityUtils;
 import org.junit.jupiter.api.BeforeAll;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public abstract class E2ETestBase {
+
     private static final Logger logger = LoggerFactory.getLogger(E2ETestBase.class);
 
     private static Tomcat tomcat;
@@ -31,6 +36,18 @@ public abstract class E2ETestBase {
     protected static Context context;
 
     protected static int port;
+
+    private static final RedirectStrategy neverRedirectStrategy = new RedirectStrategy() {
+        @Override
+        public boolean isRedirected(HttpRequest request, HttpResponse response, HttpContext context) {
+            return false;
+        }
+
+        @Override
+        public HttpUriRequest getRedirect(HttpRequest request, HttpResponse response, HttpContext context) {
+            return null;
+        }
+    };
 
     @BeforeAll
     public static synchronized void setUpClass() throws Exception {
@@ -114,10 +131,36 @@ public abstract class E2ETestBase {
         }
     }
 
+    protected SavedHttpResponse get(String path, String sessionId) throws IOException {
+        HttpGet httpGet = new HttpGet("http://localhost:" + port + path);
+        httpGet.addHeader("Cookie", "JSESSIONID=" + sessionId);
+        try (CloseableHttpClient client = HttpClients.custom()
+                .setRedirectStrategy(neverRedirectStrategy)
+                .build();
+             CloseableHttpResponse response = client.execute(httpGet)) {
+            response.getStatusLine();
+            response.getAllHeaders();
+            response.getEntity();
+            return new SavedHttpResponse(response.getStatusLine(), response.getAllHeaders(),
+                    EntityUtils.toString(response.getEntity()));
+        }
+    }
+
     // path should start with "/"
     protected HttpResponse post(String path, String body) throws IOException {
         try (CloseableHttpClient client = HttpClients.createDefault()) {
             HttpPost request = new HttpPost("http://localhost:" + port + path);
+            request.setHeader("Content-Type", "application/x-www-form-urlencoded");
+            request.setEntity(new StringEntity(body));
+            CloseableHttpResponse response = client.execute(request);
+            return response;
+        }
+    }
+
+    protected HttpResponse post(String path, String body, String sessionId) throws IOException {
+        try (CloseableHttpClient client = HttpClients.createDefault()) {
+            HttpPost request = new HttpPost("http://localhost:" + port + path);
+            request.addHeader("Cookie", "JSESSIONID=" + sessionId);
             request.setHeader("Content-Type", "application/x-www-form-urlencoded");
             request.setEntity(new StringEntity(body));
             CloseableHttpResponse response = client.execute(request);
