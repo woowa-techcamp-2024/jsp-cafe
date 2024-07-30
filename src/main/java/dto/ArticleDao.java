7b16bf427ef03d1d15ad791f9c8f4556f8110b7a
hek-git <ghddmsrl1@naver.com>
2024-07-30 13:50:37 +0900

    refactor: ArticleDao 클래스 변경 - User 객체를 직접 갖도록 변경 - setter 삭제

--- src/main/java/dto/ArticleDao.java
+++ src/main/java/dto/ArticleDao.java
@@ -1,47 +1,29 @@
 package dto;
 
+import domain.User;
+
 public class ArticleDao {
 
-    private String writer;
+    private User writer;
     private String title;
     private String content;
-    private String created;
 
-    public ArticleDao(String writer, String title, String content) {
+    public ArticleDao(User writer, String title, String content) {
         this.writer = writer;
         this.title = title;
         this.content = content;
     }
 
-    public String getWriter() {
+    public User getWriter() {
         return writer;
-    }
-
-    public void setWriter(String writer) {
-        this.writer = writer;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getContent() {
         return content;
     }
 
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getCreated() {
-        return created;
-    }
-
-    public void setCreated(String created) {
-        this.created = created;
-    }
 }
