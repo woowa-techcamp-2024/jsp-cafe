ed82d11e542aaf9098958d7ec4a9ce525ed41212
KyungMin Lee <hello.tidavid@gmail.com>
2024-07-30 16:43:27 +0900

    docs: 주석 추가
    - `LoginAuthenticationFilter`와 `UserAuthenticationFilter`에 주석을 추가했습니다.

--- src/main/java/codesquad/jspcafe/filter/LoginAuthenticationFilter.java
+++ src/main/java/codesquad/jspcafe/filter/LoginAuthenticationFilter.java
@@ -8,15 +8,22 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * LoginAuthenticationFilter는 로그인을 한 사용자가 접근 불가능한 URI에 접근하는 경우를 처리하는 필터입니다 <br> 로그인 시 접근 불가능한 URI에
+ * 접근하는 경우 메인 페이지로 리다이렉트합니다.
+ */
 @WebFilter({"/users/login", "/users/signup"})
 public class LoginAuthenticationFilter extends HttpFilter {
 
-    /*
-    공통 접근 가능 URI: /resources/*, /, /index.html
-    로그인 시 접근 가능 URI: /logout, etc...
-    로그인 시 접근 불가능 URI: /users/login, /users/signup
+    /**
+     * 로그인 시 접근 불가능한 URI에 접근하는 경우 메인 페이지로 리다이렉트합니다.
+     *
+     * @param req   클라이언트가 필터에 요청한 내용을 포함하는 {@link HttpServletRequest} 객체
+     * @param res   필터가 클라이언트에게 보내는 응답을 포함하는 {@link HttpServletResponse} 객체
+     * @param chain 다음 필터 또는 리소스를 호출하는 <code>FilterChain</code>
+     * @throws IOException      이 필터의 처리 중 I/O 오류가 발생한 경우
+     * @throws ServletException 처리 중 다른 이유로 실패한 경우
      */
-
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
         throws IOException, ServletException {
--- src/main/java/codesquad/jspcafe/filter/UserAuthenticationFilter.java
+++ src/main/java/codesquad/jspcafe/filter/UserAuthenticationFilter.java
@@ -8,9 +8,22 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * UserAuthenticationFilter는 로그인을 하지 않은 사용자가 접근하는 URI에 접근하는 경우를 처리하는 필터입니다. <br> 로그인을 하지 않은 사용자가
+ * 접근하는 URI에 접근하는 경우 로그인 페이지로 리다이렉트합니다.
+ */
 @WebFilter(urlPatterns = {"/users", "/questions", "/questions/*"})
 public class UserAuthenticationFilter extends HttpFilter {
 
+    /**
+     * 로그인을 하지 않은 사용자가 접근하는 URI에 접근하는 경우 로그인 페이지로 리다이렉트합니다.
+     *
+     * @param req   클라이언트가 필터에 요청한 내용을 포함하는 {@link HttpServletRequest} 객체
+     * @param res   필터가 클라이언트에게 보내는 응답을 포함하는 {@link HttpServletResponse} 객체
+     * @param chain 다음 필터 또는 리소스를 호출하는 <code>FilterChain</code>
+     * @throws IOException      이 필터의 처리 중 I/O 오류가 발생한 경우
+     * @throws ServletException 처리 중 다른 이유로 실패한 경우
+     */
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
         throws IOException, ServletException {
