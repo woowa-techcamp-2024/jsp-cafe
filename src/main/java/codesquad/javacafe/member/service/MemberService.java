c65f15cac268bc51bee4131ea239746eaaf5af1c
seungh1024 <seungh1024@naver.com>
2024-07-30 14:10:01 +0900

    feat: 배포 설정 추가
    - 배포를 위해 ROOT.war로 패키징
    - 사용자 리스트 없는 경우 렌더링 이슈 수정

--- src/main/java/codesquad/javacafe/member/service/MemberService.java
+++ src/main/java/codesquad/javacafe/member/service/MemberService.java
@@ -12,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class MemberService {
@@ -31,6 +32,9 @@ public class MemberService {
 
     public List<MemberResponseDto> getMemberList() {
         var memberList = MemberRepository.getInstance().findAll();
+        if (Objects.isNull(memberList)) {
+            return null;
+        }
         return memberList.stream()
                 .map(member -> new MemberResponseDto(member))
                 .collect(Collectors.toList());
