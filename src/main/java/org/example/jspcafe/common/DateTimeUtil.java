6b04f6f6f43f31ba79d5591f98d47abe57fdce09
KoKimSS <seungsu8848@ajou.ac.kr>
2024-07-30 15:33:15 +0900

    refactor : LocalDateTime 을 변수로 받게 수정

--- src/main/java/org/example/jspcafe/common/DateTimeUtil.java
+++ src/main/java/org/example/jspcafe/common/DateTimeUtil.java
@@ -6,8 +6,7 @@ import java.time.format.DateTimeFormatter;
 public class DateTimeUtil {
     private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-    public static String getCurrentDateTimeString() {
-        LocalDateTime now = LocalDateTime.now();
-        return now.format(formatter);
+    public static String dateTimeToString(LocalDateTime dateTime) {
+        return dateTime.format(formatter);
     }
 }
