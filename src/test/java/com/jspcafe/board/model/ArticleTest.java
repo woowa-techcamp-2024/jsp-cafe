b48e57777c5d6fc1aa8350eed01cf26d5704a8e6
hellomatia <shuaigejp80@gmail.com>
2024-07-30 11:00:44 +0900

    feat: 수정할 경우, 제목과 내용을 변경하고 수정 시간을 현재 시간으로 변경하는 기능 추가
    - 수정 시간은 현재 시간으로 변경한다.
    - 수정된 새로운 객체를 반환한다.

--- src/test/java/com/jspcafe/board/model/ArticleTest.java
+++ src/test/java/com/jspcafe/board/model/ArticleTest.java
@@ -61,4 +61,20 @@ class ArticleTest {
         assertThrows(IllegalArgumentException.class,
                 () -> new Article(id, title, nickname, content, now, now));
     }
+
+    @Test
+    void 수정할_경우_내용을_수정하고_수정날짜를_현재날짜로_변경한다() {
+        // Given
+        Article article = new Article("testId", "testTitle", "testNickname", "test test...",
+                LocalDateTime.now().minusDays(2), LocalDateTime.now().minusDays(2));
+
+        // When
+        Article updateArticle = article.update("updateTitle", "updateContent");
+
+        // Then
+        assertEquals(article.id(), updateArticle.id());
+        assertEquals("updateTitle", updateArticle.title());
+        assertEquals("updateContent", updateArticle.content());
+        assertEquals(LocalDateTime.now().getDayOfYear(), updateArticle.updateAt().getDayOfYear());
+    }
 }
