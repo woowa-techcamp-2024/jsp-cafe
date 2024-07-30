908331cc83a8221a1a8385e7b498b9c5ad9efef1
hek-git <ghddmsrl1@naver.com>
2024-07-30 13:53:40 +0900

    feat: 게시글 작성 권한부여 구현 - 로그인하지 않은 사용자가 글쓰기 페이지에 접근할 경우 로그인 페이지로 redirect

--- src/main/java/servlet/QuestionServlet.java
+++ src/main/java/servlet/QuestionServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import domain.Article;
+import domain.User;
 import dto.ArticleDao;
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletContext;
@@ -9,9 +10,11 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import service.ArticleService;
+import utils.AuthUtils;
 
 import java.io.IOException;
 
@@ -29,24 +32,38 @@ public class QuestionServlet extends HttpServlet {
     }
 
     @Override
+    // 1. 미로그인시 로그인 페이지로 이동
+    // 2. question?id=5 형태의 경우 id에 해당하는 게시글을 보여준다.
+    // 3. 없을 경우 새로운 게시글을 작성할 수 있는 form을 보여준다.
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String id = req.getParameter("id");
-        if(id == null) {
-            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+        HttpSession session = req.getSession(false);
+        if (!AuthUtils.isLoginUser(session)) {
+            resp.sendRedirect("/user/login.jsp");
             return;
         }
-        Article article = articleService.findById(Long.parseLong(id));
-        req.setAttribute("article", article);
-        req.getRequestDispatcher("/qna/show.jsp").forward(req, resp);
+
+        if (id != null) {
+            Article article = articleService.findById(Long.parseLong(id));
+            req.setAttribute("article", article);
+            req.getRequestDispatcher("/qna/show.jsp").forward(req, resp);
+            return;
+        }
+
+        req.getRequestDispatcher("/qna/form.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String writer = req.getParameter("writer");
+        HttpSession session = req.getSession(false);
+        if (!AuthUtils.isLoginUser(session)) {
+            resp.sendRedirect("/user/login.jsp");
+            return;
+        }
         String title = req.getParameter("title");
         String content = req.getParameter("content");
-        log.info("{} {} {}" , writer, title, content);
-        articleService.saveArticle(new ArticleDao(writer, title, content));
+        log.info("{} {}", title, content);
+        articleService.saveArticle(new ArticleDao((User) session.getAttribute("loginMember"), title, content));
         resp.sendRedirect("/");
     }
 }
