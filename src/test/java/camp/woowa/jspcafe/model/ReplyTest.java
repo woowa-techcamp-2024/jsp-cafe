a6df60d727e0edce55c0ae816425e3b76b4eb41b
kimhyun5u <22kimhyun5u@gmail.com>
2024-07-30 15:04:34 +0900

    test: Replay 테스트 추가

---
+++ src/test/java/camp/woowa/jspcafe/model/ReplyTest.java
@@ -0,0 +1,25 @@
+package camp.woowa.jspcafe.model;
+
+import org.junit.jupiter.api.Test;
+
+class ReplyTest {
+    @Test
+    void testConstructor() {
+        // given
+        Long id = 1L;
+        String content = "content";
+        Long questionId = 1L;
+        Long writerId = 1L;
+        String writer = "writer";
+
+        // when
+        Reply reply = new Reply(id, content, questionId, writer, writerId);
+
+        // then
+        assertEquals(reply.getId(), id);
+        assertEquals(reply.getContent(), content);
+        assertEquals(reply.getQuestionId(), questionId);
+        assertEquals(reply.getWriter(), writer);
+        assertEquals(reply.getWriterId(), writerId);
+    }
+}
