5d23a54e342d918312b40cf64ee1fd77b2b00566
minseok-oh <minseok.dev@gmail.com>
2024-07-30 13:35:26 +0900

    refactor: 불필요한 메서드 제거

--- src/main/java/cafe/domain/db/UserDatabase.java
+++ src/main/java/cafe/domain/db/UserDatabase.java
@@ -3,10 +3,6 @@ package cafe.domain.db;
 import cafe.domain.util.DatabaseConnector;
 import cafe.domain.entity.User;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-
 public class UserDatabase implements Database<String, User> {
     DatabaseConnector databaseConnector;
 
@@ -17,30 +13,5 @@ public class UserDatabase implements Database<String, User> {
     @Override
     public DatabaseConnector getConnector() {
         return this.databaseConnector;
-    }
-
-    public User selectByUserId(String userid) {
-        User user = null;
-
-        String sql = "SELECT * FROM `users` WHERE `userid` = ?";
-        try (Connection connection = databaseConnector.connect();
-             PreparedStatement statement = connection.prepareStatement(sql)) {
-
-            statement.setString(1, userid);
-            try (ResultSet resultSet = statement.executeQuery()) {
-                if (resultSet.next()) {
-                    String userId = resultSet.getString("userid");
-                    String name = resultSet.getString("name");
-                    String password = resultSet.getString("password");
-                    String email = resultSet.getString("email");
-
-                    user = User.of(userId, name, password, email);
-                }
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        return user;
     }
 }
