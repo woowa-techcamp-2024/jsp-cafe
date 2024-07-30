910409a53b52aab851b174a31601c007875a11f4
kimhyun5u <22kimhyun5u@gmail.com>
2024-07-30 21:35:36 +0900

    feat: RepliesServlet.deleteReply 구현

--- src/main/java/camp/woowa/jspcafe/servlet/RepliesServlet.java
+++ src/main/java/camp/woowa/jspcafe/servlet/RepliesServlet.java
@@ -53,6 +53,46 @@ public class RepliesServlet extends HttpServlet {
         }
     }
 
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String pathInfo = req.getPathInfo();
+
+        if (pathInfo.split("/").length == 2) {
+            deleteReply(req, resp, pathInfo);
+        } else {
+            throw new CustomException(HttpStatus.METHOD_NOT_ALLOWED, "Method not allowed");
+        }
+    }
+
+    private void deleteReply(HttpServletRequest req, HttpServletResponse resp, String pathInfo) {
+        try {
+            User sessionUser = getSessionUser(req, resp);
+
+            if (sessionUser == null) { // 로그인 검증
+                resp.sendRedirect("/users/login");
+                return;
+            }
+
+            Long id = Long.parseLong(pathInfo.split("/")[1]);
+
+            replyService.deleteById(id, sessionUser.getId());
+
+            resp.setContentType("application/json");
+            resp.getWriter().write("{\"result\":\"success\"}");
+        } catch (IOException e) {
+            throw new CustomException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        }  catch (CustomException e) {
+            resp.setStatus(e.getStatusCode());
+            resp.setContentType("application/json");
+            try {
+                resp.getWriter().write("{\"message\":\"" + e.getMessage() + "\"}");
+                return;
+            } catch (IOException ex) {
+                throw new CustomException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to write response");
+            }
+        }
+    }
+
     private void createReply(HttpServletRequest req, HttpServletResponse resp) {
         try {
             User sessionUser = getSessionUser(req, resp);
