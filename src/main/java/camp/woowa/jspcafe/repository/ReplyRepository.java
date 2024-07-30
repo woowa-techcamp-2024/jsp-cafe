b9bb3a55b6f48179aa7d86807fa62450e3c0e438
kimhyun5u <22kimhyun5u@gmail.com>
2024-07-30 15:32:32 +0900

    refactor: 불필요한 public 제

--- src/main/java/camp/woowa/jspcafe/repository/ReplyRepository.java
+++ src/main/java/camp/woowa/jspcafe/repository/ReplyRepository.java
@@ -3,9 +3,9 @@ package camp.woowa.jspcafe.repository;
 import camp.woowa.jspcafe.model.Reply;
 
 public interface ReplyRepository {
-    public Long save(Reply reply);
+    Long save(Reply reply);
 
-    public Reply findById(Long id);
+    Reply findById(Long id);
 
     void deleteAll();
 }
