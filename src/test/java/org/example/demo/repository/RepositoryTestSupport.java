21344b5ef78c18f3aa7ac23dc17e088cb3278b1d
KyuWon  Kim <kyuwon0718@gmail.com>
2024-07-31 12:49:50 +0900

    feat: repository 테스트 서포트 추가

---
+++ src/test/java/org/example/demo/repository/RepositoryTestSupport.java
@@ -0,0 +1,36 @@
+package org.example.demo.repository;
+
+import org.example.demo.db.DbConfig;
+
+import static org.example.demo.db.DbConfig.createDatabaseIfNotExists;
+
+public class RepositoryTestSupport {
+    DbConfig dbConfig;
+
+    void setDb() {
+        try {
+            Class.forName("org.h2.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        String jdbcUrl = "jdbc:h2:mem:~/cafe;MODE=MySQL";
+        String user = "sa";
+        String password = "";
+
+        dbConfig = new DbConfig(jdbcUrl, user, password);
+        dbConfig.initializeSchema();
+    }
+
+    void cleanUp() {
+        try (var connection = dbConfig.getConnection();
+             var statement = connection.createStatement()) {
+            statement.execute("SET REFERENTIAL_INTEGRITY FALSE");
+            statement.execute("TRUNCATE TABLE posts");
+            statement.execute("TRUNCATE TABLE `users`");
+            statement.execute("TRUNCATE TABLE comments");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/resources/schema.sql
@@ -0,0 +1,32 @@
+CREATE TABLE IF NOT EXISTS `users`
+(
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    user_id    VARCHAR(20)  NOT NULL,
+    email      VARCHAR(255) NOT NULL,
+    password   VARCHAR(255) NOT NULL,
+    `name`     VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS posts
+(
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    writer_id  BIGINT       NOT NULL,
+    title      VARCHAR(255) NOT NULL,
+    contents   TEXT         NOT NULL,
+    is_present BOOLEAN      NOT NULL DEFAULT TRUE,
+    created_at TIMESTAMP    NOT NULL,
+    FOREIGN KEY (writer_id) REFERENCES `users` (id)
+);
+
+CREATE TABLE IF NOT EXISTS comments
+(
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    post_id    BIGINT       NOT NULL,
+    writer_id  BIGINT       NOT NULL,
+    contents   TEXT         NOT NULL,
+    is_present BOOLEAN      NOT NULL DEFAULT TRUE,
+    created_at TIMESTAMP    NOT NULL,
+    FOREIGN KEY (post_id) REFERENCES `posts` (id),
+    FOREIGN KEY (writer_id) REFERENCES `users` (id)
+);
