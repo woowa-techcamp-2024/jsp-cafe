cb81e8b84df1b262bbf49914398ceba82b7ecfa1
seminchoi <choicco89@gmail.com>
2024-07-31 17:21:27 +0900

    feat: ReplyDao 인터페이스 선언
    - ReplyDto를 반환받을 수 있는 메소드 선언

---
+++ src/main/java/codesqaud/app/dao/reply/ReplyDao.java
@@ -0,0 +1,13 @@
+package codesqaud.app.dao.reply;
+
+import codesqaud.app.dao.CommonDao;
+import codesqaud.app.dto.ReplyDto;
+import codesqaud.app.model.Reply;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ReplyDao extends CommonDao<Reply, Long> {
+    Optional<ReplyDto> findByIdAsDto(Long id);
+    List<ReplyDto> findAllAsDto();
+}
