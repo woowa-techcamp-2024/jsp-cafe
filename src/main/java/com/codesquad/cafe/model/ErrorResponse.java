d33b77b06ae3d196574da350bc260e138c0a7e7c
김수현 <i960107@naver.com>
2024-08-01 16:04:04 +0900

    feat: Put, Delete시에 json으로 응답 전달위해 모델 추가
    - 요청 성공시 redirect Url 전달위한 RedirectResponse
    - 요청 실패시 에러 메시지 전달위한 ErrorResponse

---
+++ src/main/java/com/codesquad/cafe/model/ErrorResponse.java
@@ -0,0 +1,17 @@
+package com.codesquad.cafe.model;
+
+public class ErrorResponse {
+
+    private String message;
+
+    public ErrorResponse(){}
+
+    public ErrorResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
---
+++ src/main/java/com/codesquad/cafe/model/RedirectResponse.java
@@ -0,0 +1,17 @@
+package com.codesquad.cafe.model;
+
+public class RedirectResponse {
+
+    private String redirect;
+
+    public RedirectResponse(){}
+
+    public RedirectResponse(String redirect) {
+        this.redirect = redirect;
+    }
+
+    public String getRedirect() {
+        return redirect;
+    }
+
+}
