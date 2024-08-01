f643b84a67f4db241526d019170f197f21a64f08
june-777 <wlwhswnsrl96@gmail.com>
2024-08-01 13:38:31 +0900

    feat: 게시글 삭제 기능
    동일 사용자, 동일 게시글에 대한 검증 진행

---
+++ src/main/java/woowa/camp/jspcafe/domain/exception/ReplyException.java
@@ -0,0 +1,9 @@
+package woowa.camp.jspcafe.domain.exception;
+
+public class ReplyException extends RuntimeException {
+
+    public ReplyException(String message) {
+        super(message);
+    }
+
+}
--- src/main/java/woowa/camp/jspcafe/service/ReplyService.java
+++ src/main/java/woowa/camp/jspcafe/service/ReplyService.java
@@ -2,11 +2,18 @@ package woowa.camp.jspcafe.service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 import woowa.camp.jspcafe.domain.Reply;
+import woowa.camp.jspcafe.domain.User;
+import woowa.camp.jspcafe.domain.exception.ArticleException;
+import woowa.camp.jspcafe.domain.exception.ReplyException;
+import woowa.camp.jspcafe.domain.exception.UnAuthorizationException;
+import woowa.camp.jspcafe.domain.exception.UserException;
 import woowa.camp.jspcafe.infra.time.DateTimeProvider;
 import woowa.camp.jspcafe.repository.reply.ReplyRepository;
 import woowa.camp.jspcafe.repository.user.UserRepository;
 import woowa.camp.jspcafe.service.dto.ReplyResponse;
+import woowa.camp.jspcafe.service.dto.ReplyWriteRequest;
 
 public class ReplyService {
 
@@ -36,6 +43,52 @@ public class ReplyService {
 
     public List<ReplyResponse> findReplyList(Long articleId) {
         return replyRepository.findByArticleIdWithUser(articleId);
+    }
+
+    public void deleteReply(User user, Long articleId, Long replyId) {
+        Reply reply = findReply(replyId);
+        User replier = findReplier(reply.getUserId());
+
+        validateAuthorization(user, replier);
+        validateSameArticle(articleId, reply);
+
+        replyRepository.softDeleteById(reply.getReplyId(), dateTimeProvider.getNowAsLocalDateTime());
+    }
+
+//    // TODO
+//    public void updateReply(User user, ReplyUpdateRequest replyUpdateRequest) {
+//        Reply reply = findReply(replyUpdateRequest.replyId());
+//        User replier = findReplier(user.getId());
+//
+//        validateAuthorization(user, replier);
+//        validateSameArticle(replyUpdateRequest.articleId(), reply);
+//
+//        Reply updateReply = Reply.update(reply, replyUpdateRequest.content(), dateTimeProvider.getNowAsLocalDateTime());
+//        replyRepository.update(updateReply);
+//    }
+
+    private Reply findReply(Long replyId) {
+        return replyRepository.findById(replyId)
+                .orElseThrow(() -> new ReplyException("Reply not found : " + replyId));
+    }
+
+    private User findReplier(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new UserException("User with id " + userId + " not found"));
+    }
+
+    private void validateAuthorization(User user, User replier) {
+        String userEmail = user.getEmail();
+        String authorEmail = replier.getEmail();
+        if (!userEmail.equals(authorEmail)) {
+            throw new UnAuthorizationException("게시글 수정은 작성자의 이메일과 동일해야 합니다. %s, %s".formatted(userEmail, authorEmail));
+        }
+    }
+
+    private void validateSameArticle(Long articleId, Reply reply) {
+        if (!Objects.equals(reply.getArticleId(), articleId)) {
+            throw new ArticleException("편집하려는 댓글의 게시글 id와 일치하지 않습니다. %s %s".formatted(articleId, reply.getArticleId()));
+        }
     }
 
 }
