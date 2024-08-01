27170bbe77ecb0d251af1dd2d155dd3c10f0fa23
seminchoi <choicco89@gmail.com>
2024-08-01 10:32:39 +0900

    feat: UncheckedSQlException 예외 구현

---
+++ src/main/java/codesqaud/app/db/exception/UncheckedSQLException.java
@@ -0,0 +1,7 @@
+package codesqaud.app.db.exception;
+
+public class UncheckedSQLException extends RuntimeException {
+    public UncheckedSQLException(String message) {
+        super(message);
+    }
+}
