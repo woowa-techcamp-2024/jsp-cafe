28c5c536820242523b46715c253816b67a5b5756
june-777 <wlwhswnsrl96@gmail.com>
2024-08-01 17:02:26 +0900

    feat: 게시글 작성자가 아닌 회원의 댓글이 존재하는지 검증하는 기능
    게시글 기준 모든 댓글을 읽어오는 구현은 대규모 데이터 환경에서 메모리 부족 문제가 발생할 수 있으므로, 추후 고도화 예정

--- src/main/java/woowa/camp/jspcafe/service/ArticleService.java
+++ src/main/java/woowa/camp/jspcafe/service/ArticleService.java
@@ -1,34 +1,40 @@
 package woowa.camp.jspcafe.service;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import woowa.camp.jspcafe.domain.Article;
+import woowa.camp.jspcafe.domain.Reply;
 import woowa.camp.jspcafe.domain.User;
 import woowa.camp.jspcafe.domain.exception.ArticleException;
 import woowa.camp.jspcafe.domain.exception.UnAuthorizationException;
 import woowa.camp.jspcafe.domain.exception.UserException;
+import woowa.camp.jspcafe.infra.time.DateTimeProvider;
 import woowa.camp.jspcafe.repository.article.ArticleRepository;
 import woowa.camp.jspcafe.repository.dto.ArticleUpdateRequest;
+import woowa.camp.jspcafe.repository.reply.ReplyRepository;
 import woowa.camp.jspcafe.repository.user.UserRepository;
 import woowa.camp.jspcafe.service.dto.ArticleDetailsResponse;
 import woowa.camp.jspcafe.service.dto.ArticlePreviewResponse;
 import woowa.camp.jspcafe.service.dto.ArticleUpdateResponse;
 import woowa.camp.jspcafe.service.dto.ArticleWriteRequest;
-import woowa.camp.jspcafe.infra.time.DateTimeProvider;
 
 public class ArticleService {
 
     private static final Logger log = LoggerFactory.getLogger(ArticleService.class);
     private final ArticleRepository articleRepository;
     private final UserRepository userRepository;
+    private final ReplyRepository replyRepository;
     private final DateTimeProvider dateTimeProvider;
 
     public ArticleService(ArticleRepository articleRepository, UserRepository userRepository,
-                          DateTimeProvider dateTimeProvider) {
+                          ReplyRepository replyRepository, DateTimeProvider dateTimeProvider
+    ) {
         this.articleRepository = articleRepository;
         this.userRepository = userRepository;
+        this.replyRepository = replyRepository;
         this.dateTimeProvider = dateTimeProvider;
     }
 
@@ -78,7 +84,8 @@ public class ArticleService {
 
     private void upHits(Article article) {
         article.upHits();
-        Article updateArticle = Article.update(article, article.getTitle(), article.getContent(), article.getUpdatedAt());
+        Article updateArticle = Article.update(article, article.getTitle(), article.getContent(),
+                article.getUpdatedAt());
         articleRepository.update(updateArticle);
     }
 
@@ -99,12 +106,26 @@ public class ArticleService {
         articleRepository.update(updateArticle);
     }
 
+    /*
+     * 요구사항
+     * - 댓글이 없는 경우 삭제가 가능하다.
+     * - 게시글에 다른 사람이 댓글을 작성한 경우, 게시글 삭제가 불가능하다.
+     * - 자기 자신만 댓글을 작성한 경우만, 게시글 삭제가 가능하다.
+     * - 이 때 게시글을 삭제할 경우 자기가 쓴 댓글도 모두 삭제된다.
+     */
     public void deleteArticle(User user, Long articleId) {
         Article article = findArticle(articleId);
         User author = findAuthor(article.getAuthorId());
         validateEditable(user, author);
 
         articleRepository.deleteById(articleId);
+    }
+
+    // FIXME: 대규모 데이터 고려 필요
+    private boolean isExistNotAuthorReply(Article article) {
+        List<Reply> replies = replyRepository.findByArticleId(article.getId());
+        return replies.stream()
+                .anyMatch(reply -> !reply.getUserId().equals(article.getAuthorId()));
     }
 
     private void validateEditable(User user, User author) {
