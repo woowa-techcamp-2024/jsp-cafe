a88295955a354525aeb570bdda1d86459a6e49b4
bellringstar <bellringstar@gmail.com>
2024-08-02 08:36:08 +0900

    fix: 불필요 코드 삭제
    사용되지 않는 코드 삭제

--- src/main/java/org/example/config/controller/MethodHandler.java
+++ src/main/java/org/example/config/controller/MethodHandler.java
@@ -7,8 +7,6 @@ import java.util.Map;
 public class MethodHandler {
     private final Object instance;
     private final Method method;
-    private Map<String, Class<?>> pathVariables = new HashMap<>();
-    private Map<String, ParameterInfo> requestParams = new HashMap<>();
 
     public MethodHandler(Object instance, Method method) {
         this.instance = instance;
@@ -23,29 +21,11 @@ public class MethodHandler {
         return method;
     }
 
-    public Map<String, Class<?>> getPathVariables() {
-        return pathVariables;
-    }
-
-    public Map<String, ParameterInfo> getRequestParams() {
-        return requestParams;
-    }
-
-    public void setPathVariables(Map<String, Class<?>> pathVariables) {
-        this.pathVariables = pathVariables;
-    }
-
-    public void setRequestParams(Map<String, ParameterInfo> requestParams) {
-        this.requestParams = requestParams;
-    }
-
     @Override
     public String toString() {
         return "MethodHandler{" +
                 "instance=" + instance +
                 ", method=" + method +
-                ", pathVariables=" + pathVariables +
-                ", requestParams=" + requestParams +
                 '}';
     }
 }
