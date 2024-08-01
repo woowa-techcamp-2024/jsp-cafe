6100eac2c39be1bc9a258690ed34092764a9d1d1
donghar <donghar@naver.com>
2024-08-01 20:40:56 +0900

    [Feat] CommentDao 구현
    `+` CommentDao.findAllByArticleId
    `+` CommentDao.updateStatus

--- src/main/java/codesquad/domain/comment/CommentDao.java
+++ src/main/java/codesquad/domain/comment/CommentDao.java
@@ -1,4 +1,9 @@
 package codesquad.domain.comment;
 
+import java.util.List;
+
 public interface CommentDao {
+    List<Comment> findAllByArticleId(Long articleId);
+
+    void updateStatus(List<Comment> comments, Status status);
 }
