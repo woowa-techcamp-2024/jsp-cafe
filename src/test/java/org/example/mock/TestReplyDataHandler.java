6e6e8b533c7b381278346434f8168d5909a963da
huiseung <whtjddn75@gmail.com>
2024-08-02 13:53:32 +0900

    [test] 기능 구현이 바뀌면서 테스트 수정

---
+++ src/test/java/org/example/mock/TestReplyDataHandler.java
@@ -0,0 +1,27 @@
+package org.example.mock;
+
+import java.util.List;
+import org.example.data.ReplyDataHandler;
+import org.example.domain.Reply;
+
+public class TestReplyDataHandler implements ReplyDataHandler {
+    @Override
+    public Reply insert(Reply reply) {
+        return null;
+    }
+
+    @Override
+    public Reply update(Reply reply) {
+        return null;
+    }
+
+    @Override
+    public Reply findByReplyId(Long replyId) {
+        return null;
+    }
+
+    @Override
+    public List<Reply> findAllByArticleId(Long articleId) {
+        return List.of();
+    }
+}
