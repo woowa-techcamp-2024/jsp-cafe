941b03fdaa1ad5f17570e1b7e1df921bc4f7d170
seminchoi <choicco89@gmail.com>
2024-08-02 13:20:34 +0900

    feat: ReplyService.handleReplyCreation api로 동작하도록 구현 변경
    - jackson의 ObjectMapper 사용

--- src/main/java/codesqaud/app/service/ReplyService.java
+++ src/main/java/codesqaud/app/service/ReplyService.java
@@ -2,16 +2,19 @@ package codesqaud.app.service;
 
 import codesqaud.app.AuthenticationManager;
 import codesqaud.app.dao.reply.ReplyDao;
+import codesqaud.app.dto.ReplyDto;
 import codesqaud.app.exception.HttpException;
 import codesqaud.app.model.Reply;
 import codesqaud.app.model.User;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.InputStream;
 
-import static jakarta.servlet.http.HttpServletResponse.SC_FORBIDDEN;
-import static jakarta.servlet.http.HttpServletResponse.SC_NOT_FOUND;
+import static jakarta.servlet.http.HttpServletResponse.*;
 
 public class ReplyService {
     private final ReplyDao replyDao;
@@ -24,13 +27,24 @@ public class ReplyService {
        POST Handlers
      */
     public void handleReplyCreation(HttpServletRequest req, HttpServletResponse resp, Long articleId) throws IOException {
-        String contents = req.getParameter("contents");
+        ObjectMapper objectMapper = new ObjectMapper();
+        InputStream inputStream = req.getInputStream();
+        JsonNode jsonNode = objectMapper.readTree(inputStream);
+
+        String contents = jsonNode.get("contents").asText();
 
         User loginUser = AuthenticationManager.getLoginUserOrElseThrow(req);
         Reply reply = new Reply(contents, articleId, loginUser.getId());
         replyDao.save(reply);
 
-        resp.sendRedirect("/qna/" + articleId);
+        ReplyDto replyDto = replyDao.findByIdAsDto(reply.getId()).orElseThrow(() -> new HttpException(SC_INTERNAL_SERVER_ERROR));
+
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setStatus(HttpServletResponse.SC_CREATED);
+
+        String jsonResponse = objectMapper.writeValueAsString(replyDto);
+        resp.getWriter().write(jsonResponse);
     }
 
     /*
