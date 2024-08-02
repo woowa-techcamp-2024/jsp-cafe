c4474a98ffcad9196eb3f3c3fc474d48ed1ed86c
kimhyun5u <22kimhyun5u@gmail.com>
2024-08-02 13:00:26 +0900

    refactor: 생성 날짜 추가

--- src/main/java/camp/woowa/jspcafe/model/Question.java
+++ src/main/java/camp/woowa/jspcafe/model/Question.java
@@ -1,26 +1,30 @@
 package camp.woowa.jspcafe.model;
 
+import java.time.LocalDateTime;
+
 public class Question {
     private final Long id;
     private String title;
     private String content;
     private final String writer;
     private final Long writerId;
+    private final LocalDateTime createdAt;
 
-    public Question(Long id, String title, String content, String writer, Long writerId) {
+    public Question(Long id, String title, String content, String writer, Long writerId, LocalDateTime createdAt) {
         this.id = id;
         this.title = title;
         this.content = content;
         this.writer = writer;
         this.writerId = writerId;
+        this.createdAt = createdAt;
     }
 
     public Question(String title, String content, Long writerId) {
-        this(null, title, content, null, writerId);
+        this(null, title, content, null, writerId, null);
     }
 
     public Question(String title, String content, String writer, long writerId) {
-        this(null, title, content, writer, writerId);
+        this(null, title, content, writer, writerId, null);
     }
 
     public Long getId() {
@@ -42,6 +46,8 @@ public class Question {
     public Long getWriterId() {
         return writerId;
     }
+
+    public LocalDateTime getCreatedAt() { return createdAt; }
 
     public void update(String updatedTitle, String updatedContent) {
         if (updatedTitle != null && !updatedTitle.isEmpty()) {
