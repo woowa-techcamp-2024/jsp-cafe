0fba6828e3a25a3fd307798990d81e0d298cb6db
miiiinju1 <gms07073@ynu.ac.kr>
2024-08-05 15:53:12 +0900

    feat: Comment API에 페이징 추가

--- src/main/java/org/example/jspcafe/comment/repository/JdbcCommentRepository.java
+++ src/main/java/org/example/jspcafe/comment/repository/JdbcCommentRepository.java
@@ -9,7 +9,6 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Component
 public class JdbcCommentRepository extends ReflectionIdFieldExtractor<Comment> implements CommentRepository  {
@@ -133,38 +132,6 @@ public class JdbcCommentRepository extends ReflectionIdFieldExtractor<Comment> i
         }
     }
 
-//    @Override
-//    public List<CommentVO> findAllByPostIdsJoinFetch(List<Long> postIds) {
-//        if (postIds.isEmpty()) {
-//            return List.of();
-//        }
-//
-//        String placeholders = postIds.stream()
-//                .map(id -> "?")
-//                .collect(Collectors.joining(","));
-//        String sql = "SELECT c.comment_id, c.post_id, c.user_id, u.nickname, c.content, c.created_at " +
-//                "FROM comments c " +
-//                "JOIN users u ON c.user_id = u.user_id " +
-//                "WHERE c.post_id IN (" + placeholders + ") " +
-//                "AND c.deleted_at IS NULL";
-//
-//        try (Connection conn = connectionManager.getConnection();
-//             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-//
-//            for (int i = 0; i < postIds.size(); i++) {
-//                pstmt.setLong(i + 1, postIds.get(i));
-//            }
-//
-//            try (ResultSet rs = pstmt.executeQuery()) {
-//                return mapResultSetToEntities(rs);
-//            }
-//
-//        } catch (SQLException e) {
-//            throw new RuntimeException("Comment 조회 중 오류 발생", e);
-//        }
-//    }
-
-
     private List<CommentVO> mapResultSetToEntities(ResultSet rs) throws SQLException {
         List<CommentVO> comments = new ArrayList<>();
         while (rs.next()) {
@@ -191,16 +158,20 @@ public class JdbcCommentRepository extends ReflectionIdFieldExtractor<Comment> i
     }
 
     @Override
-    public List<CommentVO> findCommentsJoinUser(Long postId) {
+    public List<CommentVO> findCommentsJoinUser(Long postId, int limit, int offset) {
         String sql = "SELECT c.comment_id, c.post_id, c.user_id, u.nickname, c.content, c.created_at " +
                 "FROM comments c " +
                 "JOIN users u ON c.user_id = u.user_id " +
-                "WHERE c.post_id = ? AND c.deleted_at IS NULL";
+                "WHERE c.post_id = ? AND c.deleted_at IS NULL " +
+                "ORDER BY c.created_at DESC " +
+                "LIMIT ? OFFSET ?";
 
         try (Connection conn = connectionManager.getConnection();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
             pstmt.setLong(1, postId);
+            pstmt.setInt(2, limit);
+            pstmt.setInt(3, offset);
 
             try (ResultSet rs = pstmt.executeQuery()) {
                 return mapResultSetToEntities(rs);
