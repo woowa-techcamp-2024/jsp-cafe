dee5f8da80eda2f9123552883a824bfa6226aadc
huiseung <whtjddn75@gmail.com>
2024-08-05 20:40:41 +0900

    [chore] 더미 데이터 데이터베이스 삽입 코드 추가

--- init.sql
+++ init.sql
@@ -1,5 +1,7 @@
 -- 기존 테이블 제거
-use mydb;
+USE mydb;
+
+SET GLOBAL local_infile = 1;
 
 DROP TABLE IF EXISTS articles;
 DROP TABLE IF EXISTS users;
@@ -34,3 +36,29 @@ CREATE TABLE IF NOT EXISTS replies(
     alive_status VARCHAR(20) NOT NULL,
     created_dt DATETIME NOT NULL
 );
+
+-- Load data from CSV file
+LOAD DATA INFILE '/var/lib/mysql-files/articles.csv'
+INTO TABLE articles
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(article_id, user_id, title, content, author, alive_status, created_dt);
+
+LOAD DATA INFILE '/var/lib/mysql-files/users.csv'
+INTO TABLE users
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(user_id, email, nickname, password, created_dt);
+
+LOAD DATA INFILE '/var/lib/mysql-files/replies.csv'
+INTO TABLE replies
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(reply_id, user_id, article_id, author, comment, alive_status, created_dt);
+--
