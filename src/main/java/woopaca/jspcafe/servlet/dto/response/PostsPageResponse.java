feb6730029927957f151bfd6dbd618c654901175
jcw1031 <jcw001031@gmail.com>
2024-08-06 16:52:10 +0900

    refactor: 페이지 응답으로 DTO 변경
    - 게시글 리스트와 페이지 정보, 전체 게시글 개수를 포함

--- src/main/java/woopaca/jspcafe/servlet/dto/response/PostsPageResponse.java
+++ src/main/java/woopaca/jspcafe/servlet/dto/response/PostsPageResponse.java
@@ -1,14 +1,30 @@
 package woopaca.jspcafe.servlet.dto.response;
 
 import woopaca.jspcafe.model.Post;
-import woopaca.jspcafe.model.User;
 
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
-public record PostsResponse(Long id, String title, String writer, String writtenAt, int viewCount) {
+public record PostsPageResponse(List<PostsResponse> posts, Page page, int totalPosts) {
 
-    public static PostsResponse of(Post post, User user) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
-        return new PostsResponse(post.getId(), post.getTitle(), user.getNickname(), post.getWrittenAt().format(formatter), post.getViewCount());
+    public static PostsPageResponse of(List<Post> posts, List<String> writers, int currentPage, int totalPage, int totalPosts) {
+        Page page = new Page(totalPage, currentPage);
+        List<PostsResponse> postsResponseList = new ArrayList<>();
+        for (int i = 0; i < posts.size(); i++) {
+            postsResponseList.add(PostsResponse.of(posts.get(i), writers.get(i)));
+        }
+        return new PostsPageResponse(postsResponseList, page, totalPosts);
+    }
+
+    public record PostsResponse(Long id, String title, String writer, String writtenAt, int viewCount) {
+
+        public static PostsResponse of(Post post, String writer) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+            return new PostsResponse(post.getId(), post.getTitle(), writer, post.getWrittenAt().format(formatter), post.getViewCount());
+        }
+    }
+
+    public record Page(int totalPage, int currentPage) {
     }
 }
