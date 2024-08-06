167339edf01764c9657be09eca5f8d95f6a2ea67
june-777 <wlwhswnsrl96@gmail.com>
2024-08-06 20:25:43 +0900

    refactor: 메서드 이름 변경 및 불변 객체로 PageVO 수정

--- src/main/java/woowa/camp/jspcafe/service/PageVO.java
+++ src/main/java/woowa/camp/jspcafe/service/PageVO.java
@@ -6,40 +6,40 @@ public class PageVO {
     public static final int MAX_PAGE_COUNT = 10;   // 페이지 버튼의 최대 개수
 
     private final long currentPage;
-    private long firstPage;
-    private long lastPage;
-    private long totalPageCount;
+    private final long firstPage;
+    private final long lastPage;
+    private final long totalPageCount;
 
-    private boolean previousPageExist;
-    private boolean nextPageExist;
+    private final boolean previousPageExist;
+    private final boolean nextPageExist;
 
     public PageVO(long currentPage, long totalRowCount) {
         this.currentPage = currentPage;
-        setTotalPageCount(totalRowCount);
-        setFirstPage();
-        setLastPage();
-        setPreviousPageExist();
-        setNextPageExist();
+        this.totalPageCount = calculateTotalPageCount(totalRowCount);
+        this.firstPage = calculateFirstPage();
+        this.lastPage = calculateLastPage();
+        this.previousPageExist = calculatePreviousPageExist();
+        this.nextPageExist = calculateNextPageExist();
     }
 
-    private void setTotalPageCount(long totalRowCount) {
-        totalPageCount = (totalRowCount + MAX_ROW_COUNT_PER_PAGE - 1) / MAX_ROW_COUNT_PER_PAGE;
+    private long calculateTotalPageCount(long totalRowCount) {
+        return (totalRowCount + MAX_ROW_COUNT_PER_PAGE - 1) / MAX_ROW_COUNT_PER_PAGE;
     }
 
-    private void setFirstPage() {
-        firstPage = (currentPage - 1) / MAX_PAGE_COUNT * MAX_PAGE_COUNT + 1;
+    private long calculateFirstPage() {
+        return (currentPage - 1) / MAX_PAGE_COUNT * MAX_PAGE_COUNT + 1;
     }
 
-    private void setLastPage() {
-        lastPage = Math.min(totalPageCount, firstPage + MAX_PAGE_COUNT - 1);
+    private long calculateLastPage() {
+        return Math.min(totalPageCount, firstPage + MAX_PAGE_COUNT - 1);
     }
 
-    private void setPreviousPageExist() {
-        previousPageExist = firstPage > 1L;
+    private boolean calculatePreviousPageExist() {
+        return firstPage > 1L;
     }
 
-    private void setNextPageExist() {
-        nextPageExist = lastPage < totalPageCount;
+    private boolean calculateNextPageExist() {
+        return lastPage < totalPageCount;
     }
 
     public long getCurrentPage() {
